93637f2653fc4707484597a6c41d4587b0138482
Ace0x <59582360+Ace0x@users.noreply.github.com>
2023-04-09 19:08:55 -0700

    added working test model

--- AI_Model/model.py
+++ AI_Model/model.py
@@ -2,53 +2,39 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-from data import dataP
+#from rMatrix import rMatrix
+from sklearn.model_selection import train_test_split
 
-num_users, num_movies,x_train,x_val,y_train,y_val = dataP("ml-latest-small/ratings.csv")
-size = 50
 
-class RecommenderNet(tf.keras.Model):
-    def __init__(self, num_users, num_movies, embedding_size, **kwargs):
-        super(RecommenderNet, self).__init__(**kwargs)
-        self.num_users = num_users
-        self.num_movies = num_movies
-        self.embedding_size = embedding_size
-        self.user_embedding = tf.keras.layers.Embedding(
-            num_users,
-            embedding_size,
-            embeddings_initializer="he_normal",
-            embeddings_regularizer= tf.keras.regularizers.l2(1e-6),
-        )
-        self.user_bias =  tf.keras.layers.Embedding(num_users, 1)
-        self.movie_embedding =  tf.keras.layers.Embedding(
-            num_movies,
-            embedding_size,
-            embeddings_initializer="he_normal",
-            embeddings_regularizer= tf.keras.regularizers.l2(1e-6),
-        )
-        self.movie_bias =  tf.keras.layers.Embedding(num_movies, 1)
+#num_users, num_movies,x_train,x_val,y_train,y_val = dataP("ml-latest-small/ratings.csv")
+header = ['userId', 'carId', 'rating', 'timestamp']
+df = pd.read_csv("u.data",sep='\t',names=header)
+df = df.drop('timestamp', axis=1)
+train, test = train_test_split(df, test_size=0.2, random_state=42)
+users = len(df.userId.unique())
+cars = len(df.carId.unique())
 
-    def call(self, inputs):
-        user_vector = self.user_embedding(inputs[:, 0])
-        user_bias = self.user_bias(inputs[:, 0])
-        movie_vector = self.movie_embedding(inputs[:, 1])
-        movie_bias = self.movie_bias(inputs[:, 1])
-        dot_user_movie = tf.tensordot(user_vector, movie_vector, 2)
-        x = dot_user_movie + user_bias + movie_bias
-        return tf.nn.sigmoid(x)
-    
-model = RecommenderNet(num_users, num_movies, size)
+print(users)
+print(cars)
+print(df)
+size = 5
 
-model.compile(
-    loss=tf.keras.losses.BinaryCrossentropy(),
-    optimizer=keras.optimizers.Adam(learning_rate=0.001),
-)
+car_input = tf.keras.layers.Input(shape=[1], name="Car-Input")
+car_embedding = tf.keras.layers.Embedding(cars + 1, size, name="Car-Embedding")(car_input)
+car_vec = tf.keras.layers.Flatten(name="Flatten-Cars")(car_embedding)
 
-history = model.fit(
-    x=x_train,
-    y=y_train,
-    batch_size=64,
-    epochs=5,
-    verbose='auto',
-    validation_data=(x_val, y_val),
-)
+user_input = tf.keras.layers.Input(shape=[1], name="User-Input")
+user_embedding = tf.keras.layers.Embedding(users + 1, size, name="User-Embedding")(user_input)
+user_vec = tf.keras.layers.Flatten(name="Flatten-Users")(user_embedding)
+
+conc = tf.keras.layers.Concatenate()([car_vec, user_vec])
+
+fc1 = tf.keras.layers.Dense(128, activation='relu')(conc)
+fc2 = tf.keras.layers.Dense(32, activation='relu')(fc1)
+out = tf.keras.layers.Dense(1)(fc2)
+
+model = tf.keras.Model([user_input, car_input], out)
+model.compile('adam', 'mean_squared_error')
+
+history = model.fit([train.userId, train.carId], train.rating, epochs=10, verbose=1)
+model.save("mod.h5")
