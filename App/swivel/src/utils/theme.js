8eca939714bfd37720dd5aa45b7a09e8c05bc9f7
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-04-15 20:42:26 -0600

    Create theme js

---
+++ App/swivel/src/utils/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+import { Raleway } from "next/font/google";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    contrast: {
+      main: "#111439",
+    },
+  },
+});
