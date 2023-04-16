191a9a9b3344465bd1bf21c733027914f4a23a1a
karla-mond <karla.mondragon.rosas@gmail.com>
2023-04-16 17:15:12 -0600

    versión no alineada

--- App/swivel/src/components/formRegistro.js
+++ App/swivel/src/components/formRegistro.js
@@ -5,45 +5,49 @@ import styles from "@/styles/formRegistro.module.css";
 
 export default function FormRegistro (){
     return (
-      <form className={styles.forms}>
-        <img className={styles.logo} alt="" src="/swivelLogo.svg" />
-        <b className={styles.crearCuenta}>Crear Cuenta</b>
-        <Form.Control placeholder="Nombre(s)"/>
-        <Form.Control placeholder="Apellidos"/>
-        <Form.Control
-            type="email"
-            name="email"
-            placeholder="Correo electrónico"
-        />
-        <Form.Control
-          type="password"
-          name="password"
-          placeholder="Contraseña"
-        />
-        <Form.Control
-          type="password"
-          name="password"
-          placeholder="Confirmar Contraseña"
-        />
+      <div className={styles.formulario}>
+        <form className={styles.forms}>
+          <img className={styles.logo} alt="" src="/swivelLogo.svg" />
+          <b className={styles.crearCuenta}>Crear Cuenta</b>
+          <div className={styles.datosPersonales}>
+            <Form.Control placeholder="Nombre(s)"/>
+            <Form.Control placeholder="Apellidos"/>
+          </div>
+          <Form.Control
+              type="email"
+              name="email"
+              placeholder="Correo electrónico"
+          />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+          />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Confirmar Contraseña"
+          />
 
-      <Button
-        className={styles.botnregistro}
-        name="registerButton">
-        Crear Cuenta
-      </Button>
+        <Button
+          className={styles.botnregistro}
+          name="registerButton">
+          Crear Cuenta
+        </Button>
 
-      <Button
-        className={styles.botngoogle}
-        name="loginButtonGoogle"
-        variant="secondary">
-        <img className={styles.logoIcon} alt="" src="/googleLogo.svg" />
-        Crear cuenta con Google
-      </Button>
-      
-      <i className={styles.noTienesCuentaContainer}>
-        {`¿Representas a un Grupo Automotriz? `}
-        <span className={styles.regstrateAqu}><Link href='/'>Regístrate aquí</Link></span>
-      </i>
-    </form>
+        <Button
+          className={styles.botngoogle}
+          name="loginButtonGoogle"
+          variant="secondary">
+          <img className={styles.logoIcon} alt="" src="/googleLogo.svg" />
+          Crear cuenta con Google
+        </Button>
+        
+        <i className={styles.noTienesCuentaContainer}>
+          {`¿Representas a un Grupo Automotriz? `}
+          <span className={styles.regstrateAqu}><Link href='/'>Regístrate aquí</Link></span>
+        </i>
+     </form>
+  </div>
   );
 };
--- App/swivel/src/pages/registroComprador.js
+++ App/swivel/src/pages/registroComprador.js
@@ -10,23 +10,13 @@ export default function registroComprador () {
             <FormRegistro/>
           </div>
           <div className={styles.cardwelcome}>
-            <div className={styles.cardwelcomebackground}>
-              <div className={styles.cardwelcome1}>
-                <img
-                  className={styles.clipPathGroup}
-                  alt=""
-                  src="/CardWelcome.svg"
-                />
-                <div className={styles.welcometext}>
-                  <b className={styles.bienvenidx}>Bienvenidx</b>
-                <div className={styles.crearCuenta}>
-                  Compra el auto de tus sueños a un Click
-                </div>
-              </div>
+            <div className={styles.crearCuenta}>
+                Compra el auto de <br></br>
+                tus sueños a un <br></br>
+                click
             </div>
           </div>
         </div>
       </div>
-    </div>
     );
 };
