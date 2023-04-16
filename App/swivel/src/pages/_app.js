da9444db953d47aad2445e64477dea55d71d5176
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-04-15 20:41:58 -0600

    Add bootstrap init to app, as well as mui

--- App/swivel/src/pages/_app.js
+++ App/swivel/src/pages/_app.js
@@ -1,5 +1,22 @@
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Head from "next/head";
+import { useEffect } from 'react'
+import { theme } from '../utils/theme'
+import { ThemeProvider } from '@mui/material'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap')
+  }, [])
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  ) 
 }
