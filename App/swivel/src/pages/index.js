e8d0499903c07e4c69211a021c0b84a3a8a9e6a4
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-04-15 20:42:10 -0600

    Add navbar to index

--- App/swivel/src/pages/index.js
+++ App/swivel/src/pages/index.js
@@ -3,18 +3,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Link href='/'>
-        <Image src='/logo.png' alt='Swivel' width={200} height={200} />
-      </Link>
-      <Link href='/auth/register'>Regístrate</Link>
-      <Link href='/auth/login'>Inicia sesión</Link>
-      <Link href='/automotive_group/settings'>Ajustes GA</Link>
+      <Navbar /> 
     </>
   )
 }
