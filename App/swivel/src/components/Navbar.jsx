d3afb667d3e6225ddc39d46ff31eae039d17d65c
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-04-16 21:50:00 -0600

    Add logout and conditional logout button

--- App/swivel/src/components/Navbar.jsx
+++ App/swivel/src/components/Navbar.jsx
@@ -19,6 +19,7 @@ import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Link from 'next/link';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 // Constantes que contienen los nombres de las paginas y sus respectivos links
 const pages = [
@@ -35,10 +36,13 @@ const settings = [
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [auth, setAuth] = React.useState(true);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
+
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -50,6 +54,16 @@ function ResponsiveAppBar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
 
   return (
     <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none'}}>
@@ -146,65 +160,118 @@ function ResponsiveAppBar() {
             ))}
           </Box>
 
-          <Box sx={{ display: { md: 'none' } }}>
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, color: 'black' }}>
-              <MoreVertIcon/>
-            </IconButton>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <Link href={setting.link} style={{ textDecoration: 'none' }} key={setting.name}>
-                  <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
-                    <Typography 
-                      textAlign="center" 
-                      fontFamily='Raleway' 
-                      fontSize={13} 
-                      color='black'>
-                        {setting.name}
-                    </Typography>
-                  </MenuItem>
-                </Link>
-              ))}
-            </Menu>
-          </Box>
-
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            {settings.map((setting, index) => (
-              <Link href={setting.link} style={{ textDecoration: 'none' }} key={setting.name}>
-                <Button
-                  variant='contained'
-                  disableElevation
-                  color= {index === 0 ? 'secondary' : 'alternate'}
-                  className= 'mx-2 rounded-pill'
-                  size='medium'
-                  onClick={handleCloseUserMenu}
-                  sx={{ my: 2, display: 'block' }}
+          {!auth && (
+            <>
+              <Box sx={{ display: { md: 'none' } }}>
+                <IconButton onClick={handleMenu} sx={{ p: 0, color: 'black' }}>
+                  <MoreVertIcon/>
+                </IconButton>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
                 >
+                  {settings.map((setting) => (
+                    <Link href={setting.link} style={{ textDecoration: 'none' }} key={setting.name}>
+                      <MenuItem key={setting.name} onClick={handleClose}>
+                        <Typography 
+                          textAlign="center" 
+                          fontFamily='Raleway' 
+                          fontSize={13} 
+                          color='black'>
+                            {setting.name}
+                        </Typography>
+                      </MenuItem>
+                    </Link>
+                  ))}
+                </Menu>
+              </Box>
+
+              <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                {settings.map((setting, index) => (
+                  <Link href={setting.link} style={{ textDecoration: 'none' }} key={setting.name}>
+                    <Button
+                      variant='contained'
+                      disableElevation
+                      color= {index === 0 ? 'secondary' : 'alternate'}
+                      className= 'mx-2 rounded-pill'
+                      size='medium'
+                      onClick={handleClose}
+                      sx={{ my: 2, display: 'block' }}
+                    >
+                      <Typography
+                        color='black' 
+                        fontFamily='Raleway'
+                        fontSize={13}
+                      >
+                        {setting.name}
+                      </Typography>
+                    </Button>
+                  </Link>
+                ))}
+              </Box>
+            </>
+          )}
+
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenUserMenu}
+                color="inherit"
+              >
+                <AccountCircle fontSize='500' />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={handleCloseUserMenu}>
                   <Typography
                     color='black' 
                     fontFamily='Raleway'
                     fontSize={13}
                   >
-                    {setting.name}
+                    Perfil
                   </Typography>
-                </Button>
-              </Link>
-            ))}
-          </Box>
+                </MenuItem>
+                <MenuItem onClick={() => {handleCloseUserMenu; setAuth(false)}}>
+                  <Typography
+                    color='black' 
+                    fontFamily='Raleway'
+                    fontSize={13}
+                  >
+                    Cerrar Sesion
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
 
         </Toolbar>
       </Container>
