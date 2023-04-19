46a16e6ddc105d87687de1cdcee33b617a2b9548
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-04-18 21:10:01 -0600

    Basic dropdown

--- App/swivel/src/components/registro_autos_popup.js
+++ App/swivel/src/components/registro_autos_popup.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { FormControl, FormLabel, InputLabel, NativeSelect, TextField, Button, Link } from '@mui/material'
+import { FormControl, FormLabel, InputLabel, TextField, Button, Link, Select, MenuItem } from '@mui/material'
 import styles from './registro_autos_popup.module.css'
 import { FileUpload } from '@mui/icons-material'
 
@@ -30,8 +30,6 @@ export default function RegistroAutosPopup(props) {
     "Azul",
     "Verde",
     "Blanco"
-
-
   ]
 
 
@@ -40,178 +38,195 @@ export default function RegistroAutosPopup(props) {
     "No disponible"
   ]
 
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
   const handleSubmit = (event) => {
-      event.preventDefault()
+    event.preventDefault()
 
-      setMarcaError(false);
-      setModeloError(false);
-      setAnioError(false);
-      setPrecioError(false);
-      setColorError(false);
-      setCombustibleError(false);
-      setMotorError(false);
-      setTipoVehiculoError(false);
-      setDisponibilidadError(false);
+    setMarcaError(false);
+    setModeloError(false);
+    setAnioError(false);
+    setPrecioError(false);
+    setColorError(false);
+    setCombustibleError(false);
+    setMotorError(false);
+    setTipoVehiculoError(false);
+    setDisponibilidadError(false);
 
-      if (marca === '') {
-        setMarcaError(true);
-      }
-      if (modelo === '') {
-        setModeloError(true);
-      }
-      if (anio === '') {
-        setAnioError(true);
-      }
-      if (precio === '') {
-        setPrecioError(true);
-      }
-      if (color === '') {
-        setColorError(true);
-      }
-      if (combustible === '') {
-        setCombustibleError(true);
-      }
-      if (motor === '') {
-        setMotorError(true);
-      }
-      if (tipoVehiculo === '') {
-        setTipoVehiculoError(true);
-      }
-      if (disponibilidad === '') {
-        setDisponibilidadError(true);
-      }
+    if (marca === '') {
+      setMarcaError(true);
+    }
+    if (modelo === '') {
+      setModeloError(true);
+    }
+    if (anio === '') {
+      setAnioError(true);
+    }
+    if (precio === '') {
+      setPrecioError(true);
+    }
+    if (color === '') {
+      setColorError(true);
+    }
+    if (combustible === '') {
+      setCombustibleError(true);
+    }
+    if (motor === '') {
+      setMotorError(true);
+    }
+    if (tipoVehiculo === '') {
+      setTipoVehiculoError(true);
+    }
+    if (disponibilidad === '') {
+      setDisponibilidadError(true);
+    }
 
-      if (marca && anio) {
-          console.log(marca, anio)
-      }
+    if (marca && anio) {
+      console.log(marca, anio)
+    }
+
   }
 
+  const handleChangeDisponibilidad = (event) => {
+    setDisponibilidad(event.target.value);
+  }
+
+
   return (props.trigger) ? (
-    <div className = {styles.popup}>
-      <div className= {styles.popupInner}>
-        <button className= {styles.closeBtn} onClick = {() => props.setTrigger(false)}> close </button>
-        {props.children}  
+    <div className={styles.popup}>
+      <div className={styles.popupInner}>
+        <button className={styles.closeBtn} onClick={() => props.setTrigger(false)}> close </button>
+        {props.children}
 
-        <React.Fragment> 
-        <form autoComplete="off" onSubmit={handleSubmit} >
+        <React.Fragment>
+          <form autoComplete="off" onSubmit={handleSubmit} >
             <div className={styles.title}>
-            <h3>Ingresa la informacion necesaria para registrar un auto</h3>
+              <h3>Ingresa la informacion necesaria para registrar un auto</h3>
             </div>
-              <div className= {styles.formRow}>
-                <TextField 
-                    label="Marca"
-                    onChange={e => setMarca(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="text"
-                    sx={{mb: 3}}
-                    value={marca}
-                    error={marcaError}
-                 />
-                <TextField 
-                    label="Modelo"
-                    onChange={e => setModelo(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="text"
-                    value={modelo}
-                    error={modeloError}
-                    sx={{mb: 3}}
-                 />
-                 <TextField 
-                    label="Año"
-                    onChange={e => setAnio(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="text"
-                    value={anio}
-                    error={anioError}
-                    sx={{mb: 3}}
-                 />
-              </div>
-              <div className= {styles.formRow}>
-                <TextField
-                    label="Precio"
-                    onChange={e => setPrecio(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="text"
-                    value={precio}
-                    error={precioError}
-                    sx={{mb: 3}}
-                />
-                <TextField
-                    label="Color"
-                    onChange={e => setColor(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="text"
-                    value={color}
-                    error={colorError}
-                    sx={{mb: 3}}
-                />
-                <TextField
-                    label="Combustible"
-                    onChange={e => setCombustible(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="text"
-                    value={combustible}
-                    error={combustibleError}
-                    sx={{mb: 3}}
-                />
-              </div>
-              <div className= {styles.formRow}>
-                <TextField
-                    label="Motor"
-                    onChange={e => setMotor(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="text"
-                    value={motor}
-                    error={motorError}
-                    sx={{mb: 3}}
-                />
-                <TextField
-                    label="Tipo de vehiculo"
-                    onChange={e => setTipoVehiculo(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="text"
-                    value={tipoVehiculo}
-                    error={tipoVehiculoError}
-                    sx={{mb: 3}}
-                />
-                <InputLabel
-                    label="Disponibilidad"
-                    onChange={e => setDisponibilidad(e.target.value)}
-                    required
-                    variant="outlined"
-                />
-                <NativeSelect>
-                  <option value="Disponible">Disponible</option>
-                  <option value="No disponible">Agotado</option>
+            <div className={styles.formRow}>
+              <TextField
+                label="Marca"
+                onChange={e => setMarca(e.target.value)}
+                required
+                variant="outlined"
+                color="secondary"
+                type="text"
+                sx={{ mb: 3 }}
+                value={marca}
+                error={marcaError}
+              />
+              <TextField
+                label="Modelo"
+                onChange={e => setModelo(e.target.value)}
+                required
+                variant="outlined"
+                color="secondary"
+                type="text"
+                value={modelo}
+                error={modeloError}
+                sx={{ mb: 3 }}
+              />
+              <TextField
+                label="Año"
+                onChange={e => setAnio(e.target.value)}
+                required
+                variant="outlined"
+                color="secondary"
+                type="text"
+                value={anio}
+                error={anioError}
+                sx={{ mb: 3 }}
+              />
+            </div>
+            <div className={styles.formRow}>
+              <TextField
+                label="Precio"
+                onChange={e => setPrecio(e.target.value)}
+                required
+                variant="outlined"
+                color="secondary"
+                type="text"
+                value={precio}
+                error={precioError}
+                sx={{ mb: 3 }}
+              />
+              <TextField
+                label="Color"
+                onChange={e => setColor(e.target.value)}
+                required
+                variant="outlined"
+                color="secondary"
+                type="text"
+                value={color}
+                error={colorError}
+                sx={{ mb: 3 }}
+              />
+              <TextField
+                label="Combustible"
+                onChange={e => setCombustible(e.target.value)}
+                required
+                variant="outlined"
+                color="secondary"
+                type="text"
+                value={combustible}
+                error={combustibleError}
+                sx={{ mb: 3 }}
+              />
+            </div>
+            <div className={styles.formRow}>
+              <TextField
+                label="Motor"
+                onChange={e => setMotor(e.target.value)}
+                required
+                variant="outlined"
+                color="secondary"
+                type="text"
+                value={motor}
+                error={motorError}
+                sx={{ mb: 3 }}
+              />
+              <TextField
+                label="Tipo de vehiculo"
+                onChange={e => setTipoVehiculo(e.target.value)}
+                required
+                variant="outlined"
+                color="secondary"
+                type="text"
+                value={tipoVehiculo}
+                error={tipoVehiculoError}
+                sx={{ mb: 3 }}
+              />
+              <FormControl sx={{ mb: 3, width: 221 }}>
+                <InputLabel htmlFor="disponibilidad-select">Disponibilidad</InputLabel>
+                <Select
+                  id="disponibilidad-select"
+                  value={disponibilidad}
+                  onChange={handleChangeDisponibilidad}
+                >
+                  <MenuItem value={1}>Disponible</MenuItem>
+                  <MenuItem value={2}>No Disponible</MenuItem>
+                </Select>
+              </FormControl>
 
-                </NativeSelect>
-                
-              </div>
-              <div className = {styles.registerButton}>
+            </div>
+            <div className={styles.registerButton}>
               <FileUpload value={files} onChange={setFiles} />
-              </div>
-              <div className={styles.registerButton}>
-                <Button variant="outlined" color="secondary" type="submit">Registrar</Button>
-              </div>
-             
-        </form>
-        <small>Need an account? <Link to="/">Register here</Link></small>
+            </div>
+            <div className={styles.registerButton}>
+              <Button variant="outlined" color="secondary" type="submit">Registrar</Button>
+            </div>
+
+          </form>
+          <small>Need an account? <Link to="/">Register here</Link></small>
         </React.Fragment>
       </div>
     </div>
