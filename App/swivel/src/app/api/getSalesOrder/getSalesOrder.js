d7b316c530f18eac691d576a8db151d5062d1621
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-04-21 18:01:24 -0600

    Feat: Added endpoint to get all sales assigned to a seller

---
+++ App/swivel/src/app/api/getSalesOrder/getSalesOrder.js
@@ -0,0 +1,39 @@
+/* -------------------------------------------------------------------------- 
+[H-68]                                    
+Endpoint para obtener las ordenes de compra asignadas a un vendedor
+Autor: Alfredo Jeong Hyun Park
+ -------------------------------------------------------------------------- */
+import mongoose from 'mongoose'
+
+export const getSalesOrder = async (req, res) => {
+	if (req.method === 'GET' && req.body !== null) {
+		try {
+			const sellerSchema = new mongoose.Schema({
+				vendedor_id: { type: Number, required: true },
+				gerente_id: { type: Number, required: true },
+				ventas_en_proceso: { type: Number, required: true },
+				ventas_concluidas: { type: Number, required: true },
+			})
+			const Seller = mongoose.model('Seller', sellerSchema)
+
+			await mongoose.connect(process.env.MONGODB_URI, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			})
+
+			const allSalesInProgress = await Seller.find({
+				vendedor_id: req.body.vendedor_id,
+			})
+			return res
+				.status(200)
+				.json({
+					message: 'Obtuvo todas las ventas en proceso.',
+					ventasTotales: allSalesInProgress,
+				})
+		} catch (error) {
+			return res
+				.status(500)
+				.json({ message: 'Fallo al obtener las ventas en proceso.', error })
+		}
+	}
+}
