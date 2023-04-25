17ffe3b8b479104e1727fe2befd3fb3fff5f8cb2
Salvador Federico Milanes Braniff <A01029956@ITESM.mx>
2023-04-24 20:32:36 -0600

    Add chips for user feedback

--- App/swivel/src/pages/catalog.js
+++ App/swivel/src/pages/catalog.js
@@ -8,8 +8,7 @@ Catalogo de vehiculos, con sidebar de filtros
 y searchbar que emplearía elastic search.
 */
 import React, { useState, useEffect } from 'react';
-import { Grid } from '@mui/material';
-
+import { Grid, Chip } from '@mui/material';
 import Searchbar from '@/components/ui/searchbar';
 import LandingPageLayout from '@/components/landing_page_layout';
 import CatalogGrid from '@/components/catalog_grid';
@@ -19,13 +18,14 @@ import ApiDataDisplay from '@/components/api_data_display';
 export default function Catalog() {
   const [filters, setFilters] = useState(null);
   const [selectedFilters, setSelectedFilters] = useState([]);
+  const [selectedChips, setSelectedChips] = useState([]);
   const [apiData, setApiData] = useState(null);
   const [rawJson, setRawJson] = useState(null);
   const [expandedMenuItems, setExpandedMenuItems] = useState({});
 
   const fetchFilters = async () => {
     const queryString = selectedFilters.length
-      ? `?${selectedFilters.join("&").toLowerCase()}`
+      ? `?${selectedFilters.map(filter => filter.replace('modelos', 'modelo')).join("&")}`
       : "";
     const response = await fetch(
       `http://localhost:3000/api/catalogo/buscar-autos${queryString}`
@@ -41,6 +41,7 @@ export default function Catalog() {
   }, [selectedFilters]);
 
   const handleMenuItemClick = (category, item) => {
+    event.stopPropagation();
     setExpandedMenuItems(prevExpandedMenuItems => ({
       ...prevExpandedMenuItems,
       [category]: {
@@ -56,32 +57,45 @@ export default function Catalog() {
         if (filterIndex > -1) {
           newSelectedFilters.splice(filterIndex, 1);
         }
+        setSelectedChips(prevSelectedChips => prevSelectedChips.filter(chip => chip.category !== category || chip.value !== item));
       } else {
-        newSelectedFilters.push(`${category}=${item}`);
+        // remove any existing filter for this category
+        newSelectedFilters.filter(f => !f.startsWith(`${category}=`));
+        // add the new filter if it's not null
+        if (item) {
+          newSelectedFilters.push(`${category}=${item}`);
+          setSelectedChips(prevSelectedChips => {
+            const newChip = { category, value: item };
+            const isChipDuplicate = prevSelectedChips.find(chip => chip.category === newChip.category && chip.value === newChip.value);
+            if (isChipDuplicate) {
+              return prevSelectedChips;
+            } else {
+              return [...prevSelectedChips, newChip];
+            }
+          });
+        }
       }
       return newSelectedFilters;
     });
   };
 
-  const renderSubMenu = (category, subMenuItems) => {
-    return (
-      <ul className={styles.filters}>
-        {subMenuItems.map(subMenuItem => (
-          <li key={subMenuItem} className={styles.item}>
-            <label className={styles.label}>
-              <input
-                type="checkbox"
-                checked={expandedMenuItems[category]?.[subMenuItem]}
-                onChange={() => handleMenuItemClick(category, subMenuItem)}
-              />
-              <span className={styles.checkbox}></span>
-              {subMenuItem}
-            </label>
-          </li>
-        ))}
-      </ul>
-    );
-  };
+  const renderSubMenu = (category, subMenuItems) => (
+    <ul className={styles.filters}>
+      {subMenuItems.map(subMenuItem => (
+        <li key={subMenuItem} className={styles.item}>
+          <label className={styles.label}>
+            <input
+              type="checkbox"
+              checked={expandedMenuItems[category]?.[subMenuItem]}
+              onChange={() => handleMenuItemClick(category, subMenuItem)}
+            />
+            <span className={styles.checkbox}></span>
+            {subMenuItem}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
 
   return (
     <>
@@ -89,7 +103,17 @@ export default function Catalog() {
         <Grid container>
           <Grid item xs={12} sm={3}>
             <div className={styles.filterContainer}>
-              <div className={styles.filterTitle}>Agregar filtro</div>
+              <div className={styles.filterTitle}>Filtros</div>
+              {selectedChips.map((chip, index) => (
+                <Chip
+                  key={`${chip.category}-${chip.value}-${index}`}
+                  label={`${chip.category}: ${chip.value}`}
+                  onDelete={() => handleMenuItemClick(chip.category, chip.value)}
+                  color="primary"
+                  variant="outlined"
+                  className={styles.filterChip}
+                />
+              ))}
               {filters && (
                 <ul className={styles.filterList}>
                   {Object.entries(filters).map(([category, subMenuItems]) => (
@@ -109,11 +133,11 @@ export default function Catalog() {
               overflowY: 'scroll',
               maxHeight: '100vh',
             }}>
-              {/* <CatalogGrid /> */}
-              <div style={{ fontSize: '12px', margin: '10px 0' }}>
+              <div style={{ fontSize: '20px', margin: '10px 0' }}>
                 {`http://localhost:3000/api/catalogo/buscar-autos${selectedFilters.length ? `?${selectedFilters.join("&")}` : ""}`}
               </div>
               <ApiDataDisplay apiData={apiData} />
+              <CatalogGrid />
             </div>
           </Grid>
         </Grid>
