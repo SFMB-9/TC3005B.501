2aa6c9a89597ada510ce2cb5485d40bec40aee80
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2023-04-24 18:57:31 -0600

    Reorganize catalog dependencies

--- App/swivel/src/pages/catalog.js
+++ App/swivel/src/pages/catalog.js
@@ -8,58 +8,112 @@ Catalogo de vehiculos, con sidebar de filtros
 y searchbar que emplearía elastic search.
 */
 import React, { useState, useEffect } from 'react';
-import { Grid, Chip, Button } from '@mui/material';
+import { Grid } from '@mui/material';
 
 import Searchbar from '@/components/ui/searchbar';
 import LandingPageLayout from '@/components/landing_page_layout';
+import CatalogGrid from '@/components/catalog_grid';
+import styles from '@/styles/catalog.module.css';
+import ApiDataDisplay from '@/components/api_data_display';
 
 export default function Catalog() {
-  const [filters, setFilters] = useState([]);
+  const [filters, setFilters] = useState(null);
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [apiData, setApiData] = useState(null);
+  const [rawJson, setRawJson] = useState(null);
+  const [expandedMenuItems, setExpandedMenuItems] = useState({});
 
-  const handleDelete = (index) => {
-    setFilters((prevFilters) => {
-      const newFilters = [...prevFilters];
-      newFilters.splice(index, 1);
-      return newFilters;
-    });
+  const fetchFilters = async () => {
+    const queryString = selectedFilters.length
+      ? `?${selectedFilters.join("&").toLowerCase()}`
+      : "";
+    const response = await fetch(
+      `http://localhost:3000/api/catalogo/buscar-autos${queryString}`
+    );
+    const data = await response.json();
+    setFilters(data.filters);
+    setApiData(data);
+    setRawJson(JSON.stringify(data, null, 2));
   };
 
   useEffect(() => {
-    const fetchFilters = async () => {
-      const response = await fetch('http://localhost:3000/api/catalogo/buscar-autos');
-      const data = await response.json();
-      setFilters(data);
-    };
     fetchFilters();
-  }, []);
+  }, [selectedFilters]);
+
+  const handleMenuItemClick = (category, item) => {
+    setExpandedMenuItems(prevExpandedMenuItems => ({
+      ...prevExpandedMenuItems,
+      [category]: {
+        ...prevExpandedMenuItems[category],
+        [item]: !prevExpandedMenuItems[category]?.[item],
+      }
+    }));
+
+    setSelectedFilters(prevSelectedFilters => {
+      const newSelectedFilters = [...prevSelectedFilters];
+      if (expandedMenuItems[category]?.[item]) {
+        const filterIndex = newSelectedFilters.indexOf(`${category}=${item}`);
+        if (filterIndex > -1) {
+          newSelectedFilters.splice(filterIndex, 1);
+        }
+      } else {
+        newSelectedFilters.push(`${category}=${item}`);
+      }
+      return newSelectedFilters;
+    });
+  };
+
+  const renderSubMenu = (category, subMenuItems) => {
+    return (
+      <ul className={styles.filters}>
+        {subMenuItems.map(subMenuItem => (
+          <li key={subMenuItem} className={styles.item}>
+            <label className={styles.label}>
+              <input
+                type="checkbox"
+                checked={expandedMenuItems[category]?.[subMenuItem]}
+                onChange={() => handleMenuItemClick(category, subMenuItem)}
+              />
+              <span className={styles.checkbox}></span>
+              {subMenuItem}
+            </label>
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
   return (
     <>
       <LandingPageLayout>
         <Grid container>
           <Grid item xs={12} sm={3}>
-            <div style={{ padding: '10px', borderRight: '1px solid lightgray', borderTop: '1px solid lightgray'}}>
-              <Button>Aplicar filtros</Button>
-              Agregar filtro
-              {filters.map((filter, index) => (
-                <Chip
-                  key={index}
-                  label={filter}
-                  sx={{ marginRight: 1 }}
-                  onDelete={() => handleDelete(index)}
-                />
-              ))}
-
+            <div className={styles.filterContainer}>
+              <div className={styles.filterTitle}>Agregar filtro</div>
+              {filters && (
+                <ul className={styles.filterList}>
+                  {Object.entries(filters).map(([category, subMenuItems]) => (
+                    <li key={category} className={styles.filterItem}>
+                      <button className={styles.filterButton} onClick={() => handleMenuItemClick(category, null)}>{category}</button>
+                      {expandedMenuItems[category]?.[null] && renderSubMenu(category, subMenuItems)}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
           </Grid>
           <Grid item xs={12} sm={9}>
             <Searchbar />
             <div style={{
-              padding: '3%', 
+              padding: '3%',
               overflowY: 'scroll',
               maxHeight: '100vh',
             }}>
               {/* <CatalogGrid /> */}
+              <div style={{ fontSize: '12px', margin: '10px 0' }}>
+                {`http://localhost:3000/api/catalogo/buscar-autos${selectedFilters.length ? `?${selectedFilters.join("&")}` : ""}`}
+              </div>
+              <ApiDataDisplay apiData={apiData} />
             </div>
           </Grid>
         </Grid>
