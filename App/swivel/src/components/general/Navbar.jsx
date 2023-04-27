428f571c36649586a03d069508ebd9c10c593f08
Sebastián González <89799578+sebasgonvitec@users.noreply.github.com>
2023-04-26 18:38:39 -0600

    Revert "Add login integration with front"

--- App/swivel/src/components/general/Navbar.jsx
+++ App/swivel/src/components/general/Navbar.jsx
@@ -29,7 +29,7 @@ const pages = [
   { name: 'Auto-Quiz™', link: '/quiz' },
 ];
 const settings = [
-  { name: 'Registrate', link: '/auth/register_user' },
+  { name: 'Registrate', link: '/auth/signup_comprador' },
   { name: 'Inicia Sesion', link: '/auth/login_comprador' },
 ];
 
