c71cae287ea9eb2158c1693aee939e564bd428fd
Andreina Sananez <asananez2000@gmail.com>
2023-05-04 17:31:07 -0600

    Add profile dropdown layout

--- App/swivel/src/pages/providers/seller/seller_landing.js
+++ App/swivel/src/pages/providers/seller/seller_landing.js
@@ -1,13 +1,22 @@
-
 export default function SellerLandingPage({ children }) {
   return (
     <>
-      {/* <nav style={{backgroundColor: 'grey', padding: '20px'}}>
-        <a href="#" style={{ padding: '1250px'}}>Profile</a>
-      </nav> */}
-      <div style={{ padding: '200px'}}>
-        <button type="button" style={{ padding: '50px', margin: '50px'}}>Solicitudes de Compra</button>
-        <button type="button" style={{ padding: '50px', margin: '50px'}}>Solicitudes de Prueba de Manejo</button>
+      <div>
+          <h5>Perfil</h5>
+        <li>
+          <a href="#">Cambiar Contraseña</a>
+        </li>
+        <li>
+          <a href="#">Logout</a>
+        </li>
+      </div>
+      <div style={{ padding: "200px" }}>
+        <button type="button" style={{ padding: "50px", margin: "50px" }}>
+          Solicitudes de Compra
+        </button>
+        <button type="button" style={{ padding: "50px", margin: "50px" }}>
+          Solicitudes de Prueba de Manejo
+        </button>
       </div>
     </>
   );
