2a50a99b4d58adb6d22993b3cb9eac2ed2f8d072
Do Hyun Nam <dollis2002@gmail.com>
2023-05-14 12:05:53 -0600

    Upload docs endpoint

---
+++ App/swivel/src/pages/api/resumen-compra/subir-documentos.js/upload.js
@@ -0,0 +1,62 @@
+import admin from 'firebase-admin';
+import formidable from 'formidable';
+
+/**
+ * This is a Firebase Storage upload endpoint.
+ * 
+ * It expects a POST request with a file in the body.
+ * 
+ * Se tiene que mandar como body la informacion de un <form> con el atributo enctype="multipart/form-data"
+ * 
+ * @catlikeflyer
+ */
+
+const serviceAccount = require('path/to/serviceAccountKey.json');
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'your-firebase-storage-bucket',
+  });
+}
+
+const bucket = admin.storage().bucket();
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to process file upload.' });
+      }
+
+      const file = files.file;
+      const fileBuffer = await fs.promises.readFile(file.path);
+
+      const fileName = `${file.name}_${Date.now()}`;
+      const fileUpload = bucket.file(fileName);
+
+      const stream = fileUpload.createWriteStream({
+        metadata: {
+          contentType: file.type,
+        },
+      });
+
+      stream.on('error', (error) => {
+        console.error(error);
+        return res.status(500).json({ error: 'Failed to upload file to Firebase Storage.' });
+      });
+
+      stream.on('finish', () => {
+        const url = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
+        return res.status(200).json({ url });
+      });
+
+      stream.end(fileBuffer);
+    });
+  } else {
+    res.status(405).json({ error: 'Only POST requests are allowed.' });
+  }
+}
