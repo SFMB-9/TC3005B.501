c2a4717cf3b9e389b1fb8216f769596f2cc61159
Salvador Federico Milanes Braniff <A01029956@ITESM.mx>
2023-05-14 21:30:36 -0600

    Tweak custom navbars displays

--- App/swivel/src/components/general/custom_navbar.jsx
+++ App/swivel/src/components/general/custom_navbar.jsx
@@ -1,18 +1,19 @@
-import { AppBar, Box, Toolbar, Container, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
 
 // Generic Navbar which allows for customization
-export default function CustomNavbar({ home='/', elems_left, elems_right }) {
+export default function CustomNavbar({ home = '/', elems_left, elems_right, black=false }) {
   return (
-    <AppBar position="static" style={{ backgroundColor: '#1F1F1F'}}>
-      <Container maxWidth="xl">
+    // If black, then the background color is black, otherwise it is white
+    <AppBar position="static" style={{ backgroundColor: black ? '#000' : '#1F1F1F', boxShadow: 'none' }}>
+      {/* <Container maxWidth="xl"> */}
         <Toolbar>
           {/* Logo large width */}
           <Box
             sx={{
               display: { xs: "none", md: "flex" },
-              mr: 1,
+              mr: 6,
             }}
           >
             <Link href={home}>
@@ -34,44 +35,51 @@ export default function CustomNavbar({ home='/', elems_left, elems_right }) {
               alignItems: "center",
               justifyContent: "center",
             }}
-          >   
+          >
             <Link href="/">
               <Image src="/swivel_logo_white.svg" alt="Logo" width={120} height={30} />
             </Link>
           </Box>
+          {/* Left-most elements */}
           <Box
             sx={{
               flexGrow: 1,
               display: { xs: "none", md: "flex" },
-              justifyContent: "space-evenly",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              marginLeft: "25px", // Adjust the value to move the section more to the left
             }}
           >
-            <div>
-              {elems_left &&
-                elems_left.map((element, index) => (
-                  <div key={index}>
-                    {element}
-                  </div>
-                ))}
+            <div
+              style={{
+                display: "flex",
+                gap: "60px", // Adjust the value to increase or decrease the spacing
+              }}
+            >
+              {elems_left && elems_left.map((element, index) => element)}
             </div>
           </Box>
+          {/* Right-most elements */}
           <Box
             sx={{
               flexGrow: 1,
               display: { xs: "none", md: "flex" },
-              justifyContent: "space-evenly",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              marginRight: "16px", // Adjust the value to move the section more to the right
             }}
           >
-            {elems_right &&
-              elems_right.map((element, index) => (
-                <div key={index}>
-                  {element}
-                </div>
-              ))
-            }
+            <div
+              style={{
+                display: "flex",
+                gap: "60px", // Adjust the value to increase or decrease the spacing
+              }}
+            >
+              {elems_right && elems_right.map((element, index) => element)}
+            </div>
           </Box>
         </Toolbar>
-      </Container>
+      {/* </Container> */}
     </AppBar>
   );
 }
