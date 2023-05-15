767e39064b6414fcfe512a15a842469e2c037aee
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-05-15 05:10:37 -0600

    Add request details page

--- App/swivel/src/pages/providers/seller/view_req.js
+++ App/swivel/src/pages/providers/seller/view_req.js
@@ -1,10 +1,17 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
-import SellerNavbar from '@/components/providers/seller/seller_navbar';
+/*
+This page displays the details of a request
+Author: Mateo Herrera
+*/
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import DataTable from "@/components/general/Table";
+import { Select, MenuItem, Button, Typography } from "@mui/material";
+import SellerNavbar from "@/components/providers/seller/seller_navbar";
+import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
 
 const RequestDetails = () => {
-
   const router = useRouter();
   const [request, setRequests] = useState([]);
   const [documents, setDocuments] = useState([]);
@@ -15,41 +22,55 @@ const RequestDetails = () => {
   const fetchRequests = async () => {
     console.log("id", id);
     console.log("user_id", user_id);
-    const res = await axios.get('/api/DrivingRequestsSeller/getDrivingRequest', { params: { _id: id } });
+    const res = await axios.get(
+      "/api/DrivingRequestsSeller/getDrivingRequest",
+      { params: { _id: id } }
+    );
     const r = res.data.proceso;
-    const d = r.documentos.map(doc => ({ ...doc, comment: '' }));
+    const d = r.documentos.map((doc, index) => ({
+      ...doc,
+      comment: "",
+      _id: index,
+    }));
     setRequests(r);
     setDocuments(d);
-  }
+  };
 
   const fetchUser = async () => {
     // This function fetches the user details using the _id in the URL
-    const res = await axios.get("/api/managerProfile/managerP"
-      , { params: { id: user_id } });
+    const res = await axios.get("/api/managerProfile/managerP", {
+      params: { id: user_id },
+    });
 
     const u = res.data.userData;
     setUser(u);
-  }
+  };
 
   // This function updates the status of a document
   const updateDocumentStatus = async (_id, doc_id, status) => {
-    await axios.put('/api/DrivingRequestsSeller/updateDocumentStatus', { _id, doc_id, status });
+    await axios.put("/api/DrivingRequestsSeller/updateDocumentStatus", {
+      _id,
+      doc_id,
+      status,
+    });
     fetchRequests();
   };
 
   // This function creates a new comment for a document
-  const addNewComment = async (_id, doc_id) => {
+  const addNewComment = async (_id, doc_id, comment) => {
     const doc = documents[doc_id];
-    await axios.put('/api/DrivingRequestsSeller/updateDocumentComment', { _id, doc_id, comment: doc.comment });
+    await axios.put("/api/DrivingRequestsSeller/updateDocumentComment", {
+      _id,
+      doc_id,
+      comment: comment ? comment : doc.comment,
+    });
     fetchRequests();
   };
-
 
   useEffect(() => {
     if (id) {
       fetchRequests();
     }
-
   }, [id]);
 
   useEffect(() => {
@@ -58,99 +79,300 @@ const RequestDetails = () => {
     }
   }, [user_id]);
 
-
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
+
+  const columns = [
+    {
+      field: "descarga",
+      headerName: "",
+      headerAlign: "center",
+      align: "center",
+      type: "actions",
+      renderCell: (params) => (
+        <DownloadRoundedIcon
+          className="m-0"
+          onClick={() => console.log("clicked")}
+          sx={{
+            color: "#F55C7A",
+            "&:hover": { cursor: "pointer", color: "#F59C7A" },
+            width: 18,
+          }}
+        />
+      ),
+    },
+    {
+      field: "nombre",
+      headerName: "Nombre",
+      headerAlign: "center",
+      align: "center",
+      minWidth: 150,
+      flex: 1,
+    },
+    {
+      field: "fecha_modificacion",
+      type: "date",
+      headerName: "Ultima modificación",
+      headerAlign: "center",
+      align: "center",
+      minWidth: 150,
+      flex: 1,
+      valueFormatter: (params) =>
+        new Date(params.value).toLocaleDateString("es-ES", {
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+        }),
+    },
+    {
+      field: "status",
+      headerName: "Estatus",
+      headerAlign: "center",
+      align: "center",
+      minWidth: 150,
+      flex: 1,
+      type: "actions",
+      renderCell: (params) => (
+        console.log(params),
+        (
+          <Select
+            value={params.value}
+            onChange={(e) =>
+              updateDocumentStatus(request._id, params.row._id, e.target.value)
+            }
+            label="Status"
+            variant="standard"
+            size="small"
+            color="primary"
+            sx={{ fontFamily: "Lato", fontSize: "12px" }}
+          >
+            <MenuItem
+              sx={{ fontFamily: "Lato", fontSize: "12px" }}
+              value="En_Revision"
+            >
+              En Proceso
+            </MenuItem>
+            <MenuItem
+              sx={{ fontFamily: "Lato", fontSize: "12px" }}
+              value="Aceptado"
+            >
+              Aprobado
+            </MenuItem>
+            <MenuItem
+              sx={{ fontFamily: "Lato", fontSize: "12px" }}
+              value="Rechazado"
+            >
+              Rechazado
+            </MenuItem>
+          </Select>
+        )
+      ),
+    },
+    {
+      field: "comentarios",
+      headerName: "Comentarios",
+      headerAlign: "center",
+      align: "center",
+      minWidth: 150,
+      flex: 2,
+      type: "text",
+      editable: true,
+    },
+  ];
+
+  const rows = documents;
+  console.log(documents);
 
   return (
     // This is the page that displays the details of a request
     <>
       <SellerNavbar />
-      <div>
-        <h1>Request Details</h1>
-        {/*Car details*/}
-        <h2>Auto:</h2>
-        {request.auto ?
-          <div>
-            <p>Marca: {request.auto.marca}</p>
-            <p>Modelo: {request.auto.modelo}</p>
-            <p>Precio: {request.auto.precio}</p>
-          </div>
-          : <p>No hay auto</p>
-        }
-        {/*User details*/}
-        <h2>Cliente:</h2>
-        {user ?
-          <div>
-            <p>ID: {user._id}</p>
-            <p>Nombre: {user.name}</p>
-            <p>email: {user.email}</p>
-            <p>Telefono: {user.phone}</p>
-          </div>
-          : <p>No hay cliente</p>}
-        <div>
-          {/* This is the table that displays the documents of a request*/}
-          <h1>Request Documents</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>Document Name</th>
-                <th>Document Status</th>
-                <th>Ultima modificación</th>
-                <th>Comentarios</th>
-              </tr>
-            </thead>
-            <tbody>
-              {documents.map((document, i) => (
-                <tr key={i}>
-                  <td>{document.nombre}</td>
-                  <td>
-                    {/* This is the dropdown menu that allows the user to change the status of a document*/}
-                    <select
-                      value={document.status}
-                      onChange={(e) => updateDocumentStatus(request._id, i, e.target.value)}
-                    >
-                      <option value="En_Revision">En_Revision</option>
-                      <option value="Aceptado">Aceptado</option>
-                      <option value="Rechazado">Rechazado</option>
-                    </select>
-                  </td>
-                  <td>{document.fecha_modificacion}</td>
-                  <td>
-                    <p>{document.comentarios}</p>
-                  </td>
-                  <td>
-                    {/* This is the input field that allows the user to add a comment to a document*/}
-                    <input
-                      type="text"
-                      value={document.comment}
-                      onChange={(e) => {
-                        const newDocs = [...documents];
-                        newDocs[i].comment = e.target.value;
-                        setDocuments(newDocs);
-                      }}
-                      placeholder='Añade un comentario'
-                      onKeyDown={(e) => e.key === 'Enter' && addNewComment(request._id, i)}
-                    />
-                  </td>
-                  <td>
-                    {/* This is the button that allows the user to add a comment to a document*/}
-                    <button onClick={() => addNewComment(request._id, i)}>Añadir comentario</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+      <div className="px-lg-5 mx-xl-5">
+        <div className="section px-2">
+          <div className="px-3">
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+              className="pt-5 pb-4"
+              sx={{ fontFamily: "Lato", color: "#333333" }}
+            >
+              Solicitud de Compra
+            </Typography>
+            <div className="row">
+              <div className="col-4">
+                {/*Car details*/}
 
-          </table>
+                <ul class="list-group">
+                  <li
+                    class="list-group-item"
+                    style={{ backgroundColor: "#333333" }}
+                  >
+                    {" "}
+                    <Typography
+                      variant="h7"
+                      fontWeight="bold"
+                      sx={{ fontFamily: "Lato", color: "#FFFFFF" }}
+                    >
+                      Auto
+                    </Typography>
+                  </li>
+
+                  {request.auto ? (
+                    <>
+                      <li
+                        class="list-group-item"
+                        style={{ fontFamily: "Lato", fontSize: 11 }}
+                      >
+                        {request.auto.marca}
+                      </li>
+                      <li
+                        class="list-group-item"
+                        style={{ fontFamily: "Lato", fontSize: 11 }}
+                      >
+                        {request.auto.modelo}
+                      </li>
+                      <li
+                        class="list-group-item"
+                        style={{ fontFamily: "Lato", fontSize: 11 }}
+                      >
+                        {request.auto.precio}
+                      </li>
+                    </>
+                  ) : (
+                    <li class="list-group-item">No hay auto</li>
+                  )}
+                </ul>
+              </div>
+              <div className="col">
+                <ul class="list-group">
+                  <li
+                    class="list-group-item"
+                    style={{ backgroundColor: "#333333" }}
+                  >
+                    {" "}
+                    <Typography
+                      variant="h7"
+                      fontWeight="bold"
+                      sx={{ fontFamily: "Lato", color: "#FFFFFF" }}
+                    >
+                      Cliente
+                    </Typography>
+                  </li>
+
+                  {user ? (
+                    <>
+                      <li
+                        class="list-group-item"
+                        style={{ fontFamily: "Lato", fontSize: 11 }}
+                      >
+                        {user._id}
+                      </li>
+                      <li
+                        class="list-group-item"
+                        style={{ fontFamily: "Lato", fontSize: 11 }}
+                      >
+                        {user.name}
+                      </li>
+                      <li
+                        class="list-group-item"
+                        style={{ fontFamily: "Lato", fontSize: 11 }}
+                      >
+                        {user.email}
+                      </li>
+                      <li
+                        class="list-group-item"
+                        style={{ fontFamily: "Lato", fontSize: 11 }}
+                      >
+                        {user.phone} Phone
+                      </li>
+                    </>
+                  ) : (
+                    <li class="list-group-item">"No hay cliente</li>
+                  )}
+                  <li
+                    class="list-group-item p-0"
+                    style={{ fontFamily: "Lato", fontSize: 11 }}
+                  >
+                    <Button
+                      variant="contained"
+                      disableElevation
+                      onClick={() => console.log("clicked")}
+                      className="py-0"
+                      sx={{
+                        fontFamily: "Lato",
+                        fontWeight: "bold",
+                        width: "100%",
+                        height: "100%",
+                        backgroundColor: "#111439",
+                        "&:hover": { backgroundColor: "#333333" },
+                        borderRadius: 0,
+                        borderBottomLeftRadius: 4,
+                        borderBottomRightRadius: 4,
+                      }}
+                    >
+                      Iniciar Chat
+                    </Button>
+                  </li>
+                </ul>
+              </div>
+              {/*User details*/}
+            </div>
+          </div>
+        </div>
+        {/* This is the table that displays the documents of a request*/}
+
+        <div className="section px-2">
+          <div className="p-3 pt-2">
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              className="py-3"
+              sx={{ fontFamily: "Lato", color: "#333333" }}
+            >
+              Documentos
+            </Typography>
+            <DataTable
+              columns={columns}
+              rows={rows}
+              rowSelection={false}
+              save={true}
+              endpoint={addNewComment}
+              requiredInfo={{ rid: request._id }}
+              sx={{
+                border: 1,
+                borderColor: "#D9D9D9",
+                "& .MuiDataGrid-cell": {
+                  border: 1,
+                  borderRight: 0,
+                  borderTop: 0,
+                  borderLeft: 0,
+                  borderColor: "#D9D9D9",
+                  fontFamily: "Lato",
+                  fontWeight: 500,
+                  fontSize: "12px",
+                  color: "#333333",
+                },
+                "& .MuiDataGrid-columnHeaders": {
+                  fontFamily: "Lato",
+                  fontSize: "16px",
+                  color: "#333333",
+                  borderBottom: 0,
+                },
+                "& .MuiDataGrid-columnHeaderTitle": {
+                  fontWeight: 800,
+                },
+                "& .MuiPaginationItem-text": {
+                  fontFamily: "Lato",
+                  color: "#333333",
+                },
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
   );
 };
-
-
-
-
 
 export default RequestDetails;
