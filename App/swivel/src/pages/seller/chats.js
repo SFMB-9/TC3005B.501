0f5ae6d3707f3c490ae3c018c577bd641113c6df
Salvador Federico Milanes Braniff <A01029956@ITESM.mx>
2023-05-14 21:30:20 -0600

    Remove unused pages

--- App/swivel/src/pages/seller/chats.js
+++
@@ -1,9 +0,0 @@
-import SellerLayout from "@/components/seller/seller_layout";
-
-export default function Applications() {
-  return (
-    <>
-      <SellerLayout>Chats</SellerLayout>
-    </>
-  );
-}
