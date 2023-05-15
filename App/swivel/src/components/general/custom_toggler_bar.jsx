f0db133d8a6fe06138f5f78e397dd5ba50e53171
Salvador Federico Milanes Braniff <A01029956@ITESM.mx>
2023-05-15 03:43:32 -0600

    Tweak custom toggler bar to have component names

--- App/swivel/src/components/general/custom_toggler_bar.jsx
+++ App/swivel/src/components/general/custom_toggler_bar.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 import styles from '@/styles/custom_toggler_bar.module.css';
 
-const CustomTogglerBar = ({ components, transparent = false, stretched = false }) => {
+const CustomTogglerBar = ({ components, transparent = false, stretched = false, bold = false }) => {
   const [selectedComponentIndex, setSelectedComponentIndex] = useState(0);
 
   const handleComponentChange = (index) => {
     setSelectedComponentIndex(index);
   };
 
-  const SelectedComponent = components[selectedComponentIndex];
+  const SelectedComponent = components[selectedComponentIndex].component;
+  const componentNames = components.map(({ name }) => name);
 
   return (
     <div>
       <nav className={`${styles.bar} ${stretched ? styles.stretched : ''}`}>
-        {/* Your navbar content here */}
-        {components.map((Component, index) => (
+        {components.map(({ name }, index) => (
           <button
             key={index}
             onClick={() => handleComponentChange(index)}
             className={`${styles.button} ${selectedComponentIndex === index ? styles.selected : ''} ${
               transparent ? styles.transparent : ''
-            }`}
+            } ${bold ? styles.bold : ''}`}
           >
-            Component {index + 1}
+            {name}
           </button>
         ))}
       </nav>
