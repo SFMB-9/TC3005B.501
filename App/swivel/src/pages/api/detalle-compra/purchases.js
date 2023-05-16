54df487172e78205342bcc6cba7a288f428a5ff7
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-05-15 18:09:22 -0600

    Fix: Changed collection name

--- App/swivel/src/pages/api/detalle-compra/purchases.js
+++ App/swivel/src/pages/api/detalle-compra/purchases.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
 	if (req.method === 'GET' && req.body !== null) {
 		try {
-			const saleProcessCollection = db.collection('proceso_venta')
+			const saleProcessCollection = db.collection('procesos')
 			const result = await saleProcessCollection.find({_id: ObjectId(req.usuario_final_id)}).toArray()
 
 			return res.status(200).json({
