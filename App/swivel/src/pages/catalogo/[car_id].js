04d02650d55095b65feb35694d52e3d2391e1d5f
Sebastián González <a01029746@itesm.mx>
2023-05-16 01:34:07 -0600

    Added more car details and first version financing tool

--- App/swivel/src/pages/catalogo/[car_id].js
+++ App/swivel/src/pages/catalogo/[car_id].js
@@ -1,16 +1,29 @@
+import { set } from 'mongoose';
 import { useRouter } from 'next/router';
 import React, { useState, useEffect } from "react";
 
 // TODOs:
 // 1. Encriptar id de coche y desencriptar en el endpoint
-// 2. Agregar el resto de los campos del auto (tambien dummy)
-// 3. Agregar fotos del auto
-// 4. Agregar herramienta financiamiento
+// 2. Arreglar herramienta/seleccion financiamiento
+// 3. Agregar seleccion de extras
+// 4. Agregar seleccion de entrega
+
 
 export default function CarDetails() {
     const router = useRouter();
     const { car_id } = router.query;
     const [carDetails, setCarDetails] = useState(null);
+
+    const [selectedDownPayment, setSelectedDownPayment] = useState(null);
+    const [selectedTerm, setSelectedTerm] = useState(null);
+    const [interestRate, setInterestRate] = useState(null);
+    const [monthlyPayment, setMonthlyPayment] = useState(0);
+    const [carPrice, setCarPrice] = useState(0);
+
+    const [totalPayment, setTotalPayment] = useState(0);
+
+    // "enganche": [10,20,30,40,50],
+    // "plazo": {"12": 5.7,"24": 5.8,"36": 6.0,"48": 6.2,"60": 6.6},
 
     const fetchCarDetails = async () => {
         const response = await fetch(
@@ -20,6 +33,7 @@ export default function CarDetails() {
         const data = await response.json();
 
         setCarDetails(data.result);
+        setCarPrice(data.result.precio);
     };
 
     useEffect(() => {
@@ -29,45 +43,111 @@ export default function CarDetails() {
         fetchCarDetails();
     }, [car_id]);
 
+    const calculateMonthlyPayment = () => {
+        let carPriceWithDownPayment = carPrice - (carPrice * (selectedDownPayment / 100));
+        let monthlyPayment = (carPriceWithDownPayment / selectedTerm);
+        let monthlyPaymentTotal = monthlyPayment + (monthlyPayment * (interestRate / 100));
+
+        setMonthlyPayment(monthlyPaymentTotal.toFixed(2));
+    };
     if (carDetails != null) {
         return (
             <div>
-                <h1>Car Details</h1>
-                
-                <p>Marca: {carDetails.marca}</p>
-                <p>Modelo: {carDetails.modelo}</p>
-                <p>Año: {carDetails.año}</p>
-                <p>Precio: {carDetails.precio}</p>
-                <p>Color: {carDetails.color}</p>
-                <p>Estado: {carDetails.estado_agencia}</p>
+                <a href='/catalogo'>Regresar al catalogo</a>
+
+                <div>
+                    <p>Año: {carDetails.año}</p>
+                    <p>Marca: {carDetails.marca}</p>
+                    <p>Modelo: {carDetails.modelo}</p>
+                    <p>Ubicación: {carDetails.direccion_agencia}</p>
+                    <p>Agencia: {carDetails.nombre_agencia}</p>
+                    <p>Precio: {carDetails.precio}</p>
+                </div>
+
+                {/* Ver por que no funciona extraer colores*/}
+                <div>
+                    <h3>Fotos de auto por color</h3>
+                    {Object.values(carDetails.colores).map((color) => (
+                        <div>
+                            <p style={{ color: color.valor_hexadecimal, }}>{color.nombre}</p>
+                            {color.imagenes.map((imagen) => (
+                                <img src={imagen} alt={carDetails.modelo} />
+                            ))}
+                        </div>
+                    ))}
+                </div>
 
                 <h2>Resumen del Auto</h2>
-                
+
                 <p>Combustible: {carDetails.combustible}</p>
                 <p>Motor: {carDetails.motor}</p>
                 <p>Tipo: {carDetails.tipo_vehiculo}</p>
+                <p>Transmisión: {carDetails.transmision}</p>
+                <p>Rendimiento: {carDetails.rendimiento}</p>
+                <p>Num. Pasajeros: {carDetails.pasajeros}</p>
 
                 <h2>Características</h2>
-                    <li>Característica 1</li>
-                    <li>Característica 2</li>
-                    <li>Característica 3</li>
-                    <li>Característica 4</li>
+                {carDetails.caracteristicas.map((caracteristica) => (
+                    <li>{caracteristica}</li>
+                ))}
 
-                <h3>Extras</h3>
-                    <li>Extra 1</li>
-                    <li>Extra 2</li>
-                    <li>Extra 3</li>
-                    
-                <h3>Financiamiento</h3>
+                <h2>Extras</h2>
+                {Object.values(carDetails.extras).map((extra) => (
+                    <div>
+                        <p>{extra.titulo}: {extra.descripcion}</p>
+                        <p> Precio: {extra.precio}</p>
+                    </div>
+                ))}
 
-                <h3>Entrega</h3>
+                <h2>Financiamiento</h2>
+                <div>
+                    <div>
+                        <label>
+                            Enganche:
+                            <select value={selectedDownPayment} onChange={(e) => {
+                                setSelectedDownPayment(e.target.value);
+                            }}>
+                                <option value="">Seleccione Porcentaje de Enganche</option>
+                                {carDetails.enganche.map((option) => (
+                                    <option key={option} value={option}>
+                                        {option}
+                                    </option>
+                                ))}
+                            </select>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Plazo:
+                            <select value={selectedTerm} onChange={(e) => {
+                                setSelectedTerm(e.target.value);
+                                setInterestRate(carDetails.plazo[e.target.value]);
+                                calculateMonthlyPayment();
+                            }}>
+                                <option value="">Seleccione Plazo</option>
+                                {Object.keys(carDetails.plazo).map((option) => (
+                                    <option key={option} value={parseInt(option)}>
+                                        {option}
+                                    </option>
+                                ))}
+                            </select>
+                            <p>Tasa de Interes: {carDetails.plazo[selectedTerm]}</p>
+                        </label>
+                    </div>
+                    <div>
+                        <p>Pago Mensual: {monthlyPayment}</p>
+                        <p>Pago Total: {monthlyPayment * selectedTerm + carPrice - (carPrice * (selectedDownPayment / 100))}</p>
+                    </div>
+                </div>
+                <h2>Entrega</h2>
             </div>
+
         );
     } else {
         return (
             <div>
                 <p>Loading Car Details...</p>
             </div>
-            );
+        );
     }
 }
--- App/swivel/src/pages/catalogo/index.js
+++ App/swivel/src/pages/catalogo/index.js
@@ -10,11 +10,9 @@ y searchbar que emplearía elastic search.
 import React, { useState, useEffect } from "react";
 import { Grid, Chip, Checkbox, FormControlLabel, Typography } from "@mui/material";
 import Searchbar from "@/components/general/searchbar";
-import LandingPageLayout from "@/components/user/landing_page_layout";
-import CatalogGrid from "@/components/user/catalog_grid";
+import LandingPageLayout from "@/components/buyer/landing_page_layout";
+import CatalogGrid from "@/components/buyer/catalog_grid";
 import styles from "@/styles/catalog.module.css";
-import ApiDataDisplay from "@/components/user/api_data_display";
-import { Search } from "@mui/icons-material";
 
 export default function Catalog() {
   const [filterHeaders, setFilterHeaders] = useState(null);
