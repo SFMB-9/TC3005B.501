ab79a58e48f0e49ace5d3109c2da80979fc8a17c
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-05-15 18:33:52 -0600

    Added endpoints to get all drive tests and displayed them by status

---
+++ App/swivel/src/pages/api/prueba-manejo/test-appointments.js
@@ -0,0 +1,20 @@
+import connectToDatabase from '@/utils/mongodb'
+
+export default async function handler(req, res) {
+  const client = await connectToDatabase;
+  const db = client.db(nextjs-mongodb-demo);
+
+  if (req.method === 'GET' && req.body !== null) {
+    try {
+      const testsCollection = db.collection('proceso')  
+      const tests = await testsCollection.find({_id: ObjectId(req.usuario_final_id), tipo_proceso: 'pruebaManejo'}).toArray()
+
+      return res.status(200).json({
+        message:'Pruebas recuperadas exitosamente',
+        pruebas: tests
+      })
+    } catch (error) {
+      return res.status(400).json({error}) 
+    }
+  }
+}
