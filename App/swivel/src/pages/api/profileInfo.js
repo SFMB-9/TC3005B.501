5d7cf65b9813788bb9e3280ca12bfded03753bb1
= <=>
2023-05-15 20:16:50 -0600

    profile info by user

--- App/swivel/src/pages/api/profileInfo.js
+++ App/swivel/src/pages/api/profileInfo.js
@@ -4,12 +4,14 @@ import dbConnect from "@/config/dbConnect";
 
 export default async function handler(req, res)
 {
+    const { usuario_id } = req.query
     const client = await connectToDatabase
     const db = client.db('nextjs-mongodb-demo')
     if (req.method == "GET")
     {
         try {
-            const profileCollection = db.collection('users')
+            const profileCollection = db.collection('usuario')
+            const info = await profileCollection.findOne({_usuario_id: usuario_id})
             const profiles = await profileCollection.find({}).toArray((err, users) => {
                 console.log(profiles)
             })
@@ -20,6 +22,8 @@ export default async function handler(req, res)
             })
         }
         catch (error) {
+            console.error(error)
+            res.status(500).json({message: 'Algo salio mal.'})
             return res.status(400).json({error})
         }
     }
