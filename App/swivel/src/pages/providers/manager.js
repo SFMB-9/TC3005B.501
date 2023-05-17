96c05e7cf6ecd97e7c29491461db6b20deaa8ea9
Andreina Sananez <asananez2000@gmail.com>
2023-05-16 18:08:24 -0600

    Add Landing Page for Manager

---
+++ App/swivel/src/pages/providers/manager.js
@@ -0,0 +1,35 @@
+import { signOut } from "next-auth/react";
+
+export default function ManagerLanding({ children }) {
+  return (
+    <>
+      <div>
+        <h5>Perfil</h5>
+        <li>
+          <a href="/auth/change_password">Cambiar Contraseña</a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={() =>
+              signOut({ callbackUrl: "http://localhost:3000/auth/login" })
+            }
+          >
+            Logout
+          </a>
+        </li>
+      </div>
+      <div style={{ padding: "200px" }}>
+        <button type="button" style={{ padding: "50px", margin: "50px" }}>
+         Manejo de Catálogo 
+        </button>
+        <button type="button" style={{ padding: "50px", margin: "50px" }}>
+         Manejo de Vendedores 
+        </button>
+        <button type="button" style={{ padding: "50px", margin: "50px" }}>
+         Admin de Agencias 
+        </button>
+      </div>
+    </>
+  );
+}
