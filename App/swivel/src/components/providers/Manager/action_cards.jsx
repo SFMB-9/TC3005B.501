760dc3e9d4f5209785624134e1b2011b4e186d00
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2023-05-17 18:19:39 -0600

    Upgrades for generic components

--- App/swivel/src/components/providers/Manager/action_cards.jsx
+++ App/swivel/src/components/providers/Manager/action_cards.jsx
@@ -1,14 +1,32 @@
 import CustomCard from '@/components/general/custom_card';
 import styles from '@/styles/cards.module.css';
 
-export default function ReqCards() {
+export default function ActionsCards() {
   return (
-    <div className={styles.reqCardsContainer}>
+    <div className={styles.actionCardsContainer}>
       <CustomCard
         imageSource="/seller_sales_image.jpg"
         icon="/seller_cart_icon.svg"
         text="Solicitudes de Compra"
         href="/providers/seller/purchase_req"
+        light={false}
+        header=''
+        fiftyFifty={false}
+      />
+      <CustomCard
+        imageSource="/seller_tests_image.jpg"
+        icon="/seller_wheel_icon.svg"
+        text="Solicitudes de prueba de manejo"
+        href="/providers/seller/driving_req"
+        light={false}
+        header=''
+        fiftyFifty={false}
+      />
+      <CustomCard
+        imageSource="/seller_tests_image.jpg"
+        icon="/seller_wheel_icon.svg"
+        text="Solicitudes de prueba de manejo"
+        href="/providers/seller/driving_req"
         light={false}
         header=''
         fiftyFifty={false}
