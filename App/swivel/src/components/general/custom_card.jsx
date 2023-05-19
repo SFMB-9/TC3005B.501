e4846cf5e7e0043676facd0bc1d767243d0e31cc
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2023-05-18 19:46:30 -0600

    Define the custom card component

--- App/swivel/src/components/general/custom_card.jsx
+++ App/swivel/src/components/general/custom_card.jsx
@@ -1,38 +1,19 @@
 import styles from '@/styles/custom_card.module.css';
 import Link from 'next/link';
 
-export default function CustomReqCard({ 
-  imageSource = '/seller_sales_image.jpg', 
-  icon = null, 
-  header = '', 
-  text = '', 
-  href = '', 
-  fiftyFifty = true, 
-  light = true 
-}) {
-  const cardClassName = `${styles.card} ${light ? styles.light : ''}`;
-  const imageContainerClassName = `${styles.imageContainer} ${fiftyFifty ? styles.fiftyFifty : ''}`;
-
+export default function CustomCard({ imageSource = '/seller_sales_image.jpg', header = '', text = 'Solicitudes de Compra' }) {
   return (
-    <Link href={href}>
-      <div className={cardClassName}>
-        <div className={imageContainerClassName}>
-          <div className={styles.overlay} />
-          <img className={styles.image} src={imageSource} alt="Card" />
+    <>
+      <div className={styles.card}>
+        <div className={styles.imageContainer}>
+          <div className={styles.overlay}/>
+          <img className={styles.image} src={imageSource} alt="Card"/>
         </div>
         <div className={styles.content}>
-          {icon && (
-            <>
-              <img src={icon} className={styles.icon} />
-              <span className={styles.divider} />
-            </>
-          )}
-          <div className={styles.textContainer}>
-            <h1 className={`${styles.header} ${light ? styles.darkText : ''}`}>{header}</h1>
-            <p className={`${styles.text} ${light ? styles.darkText : ''}`}>{text}</p>
-          </div>
+          <h1 className={styles.header}>{header}</h1>
+          <p className={styles.text}>{text}</p>
         </div>
       </div>
-    </Link>
+    </>
   );
 }
