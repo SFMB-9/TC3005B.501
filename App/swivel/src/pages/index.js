b792fa439298004eff89a5370a1d1ab334811e04
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2023-05-18 19:48:39 -0600

    Rework seller landing page components

--- App/swivel/src/pages/index.js
+++ App/swivel/src/pages/index.js
@@ -1,28 +1,30 @@
 /*
-Mateo Herrera Lavalle
+Mateo Herrera Lavalle, Salvador Federico Milanés Braniff
 
 Pagina principal (landing)
 Incluye el hero, testimonials y process
 */
-import Testimonials from "@/components/buyer/testimonials";
-import Process from "@/components/buyer/process";
+// import CustomHero from "@/components/general/custom_hero";
 import BuyerLayout from "@/components/buyer/buyer_layout";
-import CustomHero from "@/components/general/custom_hero";
+import RotatingHeader from "@/components/buyer/rotating_header";
+import Process from "@/components/buyer/process";
 import CategoryViews from "@/components/buyer/category_views";
 import CarViews from "@/components/buyer/car_views";
 import SwivelExperience from "@/components/buyer/swivel_experience";
+import Testimonials from "@/components/buyer/testimonials";
 
 export default function Home() {
   return (
     <>
       <BuyerLayout>
-        <CustomHero
+        {/* <CustomHero
           titleSize='200'
           title='Compra el auto de tus sueños en línea'
           message=''
           button
           containerSize={50}
-        />
+        /> */}
+        <RotatingHeader/>
         <CategoryViews/>
         <CarViews/>
         <SwivelExperience/>
