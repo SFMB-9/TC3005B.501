24497d43d3b49a4c03d182318bbbd88e410854ce
Andreina Sananez <asananez2000@gmail.com>
2023-05-20 13:17:31 -0600

    Get messages and render them from DB

--- App/swivel/src/components/chat/AblyChatComponent.jsx
+++ App/swivel/src/components/chat/AblyChatComponent.jsx
@@ -3,6 +3,7 @@ import { useChannel } from "./AblyReactEffect";
 import styles from './AblyChatComponent.module.css';
 import { useSession } from "next-auth/react";
 import axios from "axios";
+import Ably from "ably/promises";
 
 const AblyChatComponent = () => {
 
@@ -11,12 +12,28 @@ const AblyChatComponent = () => {
   let messageEnd = null;
   const channel_name = "id1-id2";
 
-  useEffect(() => {}, [session]);
-  console.log(session.id);
-
   const [messageText, setMessageText] = useState("");
   const [receivedMessages, setMessages] = useState([]);
   const messageTextIsEmpty = messageText.trim().length === 0;
+
+  useEffect(() => {
+    // Function to fetch messages from MongoDB
+    const fetchMessages = async () => {
+        try {
+            const { data } = await axios.get('/api/chat/getMessages', {
+                params: {
+                    channelId: channel.name
+                }
+            });
+
+            const formated_msg = data.map(msg => Ably.Realtime.Message.fromEncoded({ name: "chat-message", data: msg.content, clientId: msg.sender, timestamp: msg.timestamp})); 
+            setMessages(formated_msg);
+        } catch (error) {
+            console.error('Error fetching messages:', error);
+        }
+    }
+    fetchMessages();
+    }, [session]);
 
   const [channel, ably] = useChannel(channel_name, (message) => {
     const history = receivedMessages.slice(-199);
@@ -26,7 +43,7 @@ const AblyChatComponent = () => {
 
   const sendChatMessage = async (messageText) => {
     channel.publish({ name: "chat-message", data: messageText });
-
+  
     try {
         const { data } = await axios.post('/api/chat/postMessage', {
             content: messageText,
@@ -58,7 +75,8 @@ const AblyChatComponent = () => {
   }
 
   const messages = receivedMessages.map((message, index) => {
-    const author = message.connectionId === ably.connection.id ? "me" : "other";
+    console.log(message.clientId, session.id);
+    const author = message.clientId === session.id ? "me" : "other";
     return <span key={index} className={styles.message} data-author={author}>{message.data}</span>;
   });
 
@@ -67,24 +85,26 @@ const AblyChatComponent = () => {
   });
 
   return (
-    <div className={styles.chatHolder}>
-      <div className={styles.chatText}>
-        {messages}
-        <div ref={(element) => { messageEnd = element; }}></div>
-      </div>
-      <form onSubmit={handleFormSubmission} className={styles.form}>
-        <textarea
-          ref={(element) => { inputBox = element; }}
-          value={messageText}
-          placeholder="Type a message..."
-          onChange={e => setMessageText(e.target.value)}
-          onKeyPress={handleKeyPress}
-          className={styles.textarea}
-        ></textarea>
-        <button type="submit" className={styles.button} disabled={messageTextIsEmpty}>Send</button>
-      </form>
-    </div>
-  )
+    session && (
+        <div className={styles.chatHolder}>
+          <div className={styles.chatText}>
+            {messages}
+            <div ref={(element) => { messageEnd = element; }}></div>
+          </div>
+          <form onSubmit={handleFormSubmission} className={styles.form}>
+            <textarea
+              ref={(element) => { inputBox = element; }}
+              value={messageText}
+              placeholder="Type a message..."
+              onChange={e => setMessageText(e.target.value)}
+              onKeyPress={handleKeyPress}
+              className={styles.textarea}
+            ></textarea>
+            <button type="submit" className={styles.button} disabled={messageTextIsEmpty}>Send</button>
+          </form>
+        </div>
+    )
+  );
 }
 
 export default AblyChatComponent;
--- App/swivel/src/pages/api/chat/getMessages.js
+++ App/swivel/src/pages/api/chat/getMessages.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     if (req.method === 'GET') {
         dbConnect();
 
-        const { channel } = req.body;
+        const { channel } = req.query;
         const messages = await Messages.find({channel: channel}).sort({ timestamp: 1 });
 
         res.status(200).json(messages);
