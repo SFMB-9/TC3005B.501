4fb2ae2785769a8f791b74d77d07d7510f0b019c
Salvador Federico Milanes Braniff <A01029956@ITESM.mx>
2023-05-19 23:18:38 -0600

    Add images to query header

--- App/swivel/src/components/buyer/rotating_header.jsx
+++ App/swivel/src/components/buyer/rotating_header.jsx
@@ -1,21 +1,21 @@
 import { useState, useEffect } from "react";
-import 'animate.css'
+import 'animate.css';
 import TrackVisibility from 'react-on-screen';
-import { toRotate } from './queries'; // Adjust the import path based on your project structure
+import { toRotate } from "./queries";
 import CustomHero from "../general/custom_hero";
 
 export default function RotatingHeader() {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
+  const [image, setImage] = useState('');
   const [delta, setDelta] = useState(100);
-  const period = 100;
 
   useEffect(() => {
     const tick = () => {
       let i = loopNum % toRotate.length;
-      let fullText = toRotate[i];
-  
+      let fullText = toRotate[i].text;
+
       if (isDeleting) {
         setText((prevText) => prevText.substring(0, prevText.length - 1));
       } else {
@@ -25,20 +25,26 @@ export default function RotatingHeader() {
             : fullText.substring(0, prevText.length + 1)
         );
       }
-  
+
       if (isDeleting && text === "") {
         setIsDeleting(false);
         setLoopNum((prevLoopNum) => prevLoopNum + 1);
       } else if (!isDeleting && text === fullText) {
         setIsDeleting(true);
       }
+
+      // Update the image state with the corresponding image URL
+      setTimeout(() => {
+        setImage(toRotate[i].image);
+      }, 3000);
+
       setDelta(100);
     };
-  
+
     let ticker = setInterval(() => {
       tick();
     }, delta);
-  
+
     return () => {
       clearInterval(ticker);
     };
@@ -47,14 +53,16 @@ export default function RotatingHeader() {
   return (
     <section>
       <TrackVisibility>
-      {({ isVisible }) => 
-        <div className={isVisible ? "animated__animated animate__fadeIn" : ""}>
-          <CustomHero
-            title = {text}
-            message = ''
-            searchbar
-          />
-        </div>}
+        {({ isVisible }) =>
+          <div className={isVisible ? "animated__animated animate__fadeIn" : ""}>
+            <CustomHero
+              title={text}
+              message=""
+              searchbar
+              backgroundImage={image}
+            />
+          </div>
+        }
       </TrackVisibility>
     </section>
   )
