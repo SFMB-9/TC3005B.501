649712ba87d96b71cf02d5d1f96442244717e5e4
Sebastián González <a01029746@itesm.mx>
2023-05-21 20:15:38 -0600

    Added first file of sale creation

--- App/swivel/src/pages/api/saleCreation/index.js
+++ App/swivel/src/pages/api/saleCreation/index.js
@@ -1,48 +1,21 @@
-import moongose from "moongose";
+/*
 
-// Create a Mongoose schema
-const saleCreationSchema = new moongose.Schema({
-  array_documentos: { type: Array, required: true },
-  auto_id: { type: Number, required: true },
-  usuario_final_id: { type: Number, required: true },
-  vendedor_id: { type: Number, required: true },
-  chat_id: { type: Number, required: true },
-});
+Sebastian Gonzalez Villacorta
+21/5/2023
 
-// Create a Mongoose model
-const SaleCreation = moongose.model("SaleCreation", saleCreationSchema);
+Description: Create new entry of proceso de venta in MongoDB
 
-const handler = async (req, res) => {
-  // Receive details of the purchase and manager ID
-  const { arrayDocumentos, autoId, usuarioFinalId, chatId, sellerId } =
-    req.body;
+*/
 
-  // Connect to the MongoDB database using Mongoose
-  await moongose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+// Sale creation process
 
-  try {
-    // Create the purchase
-    await SaleCreation.create({
-      array_documentos: arrayDocumentos,
-      auto_id: autoId,
-      usuario_final_id: usuarioFinalId,
-      vendedor_id: sellerId,
-      chat_id: chatId,
-    });
+//1. Assign sale to seller with least amount of sales
+//2. Obtain user data
+//3. Store current date
+//4. Assemble car json
 
-    // Respond with a success message
-    res.status(200).json({ message: "Compra creada" });
-  } catch (error) {
-    console.error(error);
-    // Respond with an error message
-    res.status(500).json({ message: "Hubo un error al crear la compra" });
-  } finally {
-    // Close the Mongoose connection
-    await mongoose.disconnect();
-  }
-};
+import mongoose from "mongoose";
 
-export default handler;
+export default async function handler(req, res) {
+
+}
