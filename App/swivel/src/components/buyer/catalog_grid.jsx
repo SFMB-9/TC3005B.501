bd8ffeb0095f9e4aeee0d81b4c237d4c614bcefa
Ana Paula Katsuda <a01025303@tec.mx>
2023-05-21 21:46:38 -0600

    changed catalog card

--- App/swivel/src/components/buyer/catalog_grid.jsx
+++ App/swivel/src/components/buyer/catalog_grid.jsx
@@ -12,47 +12,46 @@ import CarCard from "@/components/buyer/car_card";
 
 /* Función que devuelve las cartas con infrmación de los autos acomodadas y con 
 un carousel de imágenes de cada auto */
-export default function CatalogGrid({ carListing, cardType }) {
+export default function CatalogGrid({ carListing, cardType = "default" }) {
   let carList;
   if (carListing !== undefined) {
-    carList = carListing.map((car) => (
-      <Grid item xs={12} sm={12} md={6} lg={4}>
-        <CarCard
-          carUrl={`/catalogo/${car._id}`}  //{car.carUrl}
-          carImage={car._source.fotos_3d[0]} //{car.carImages}
-          // carImage=
-          // {<Carousel autoPlay={false} animation='slide' duration={300} indicatorContainerProps={{
-          //     style: {
-          //         position: 'absolute',
-          //         marginTop: '-23px',
-          //         zIndex: 1,
-          //         backgroundColor: 'rgba(217,217,214, 0.3)',
-          //     }
-          // }} indicatorIconButtonProps={{
-          //     style: {
-          //         color: 'white',
-          //     }
-          // }} activeIndicatorIconButtonProps={{
-          //     style: {
-          //         color: 'grey',
-          //     }
-          // }}>
-          //     {car.carImages.map((image) =>
-          //         <a href={image.url}>
-          //             <img src={image.image} alt={image.alt} height='30%' width='100%' />
-          //         </a>
-          //     )}
-          // </Carousel>}
-          carBrand={car._source.marca}
-          carModel={car._source.modelo}
-          carYear={car._source.año}
-          carLocation={car._source.estado_agencia} //{car.carLocation}
-          carAgency={car._source.municipio_agencia}
-          carColor={car._source.colores.length}
-          carPrice={car._source.precio}
-        />
-      </Grid>
-    ));
+    carList = carListing.map((car) => {
+      if (cardType === "default") {
+        const cardProps = {
+          default: {
+            carUrl: `/catalogo/${car._id}`,
+            carImage: car._source.fotos_3d[0],
+            carBrand: car._source.marca,
+            carModel: car._source.modelo,
+            carYear: car._source.año,
+            carLocation: car._source.estado_agencia,
+            carAgency: car._source.municipio_agencia,
+            carColor: car._source.colores.length,
+            carPrice: car._source.precio,
+          },
+          alternate: {
+            // Alternate set of props
+          },
+          // Add more sets of props as needed
+        };
+        return (
+          <Grid item xs={12} sm={12} md={6} lg={4}>
+            <CarCard
+              // carUrl={`/catalogo/${car._id}`}  //{car.carUrl}
+              // carImage={car._source.fotos_3d[0]} //{car.carImages}
+              // carBrand={car._source.marca}
+              // carModel={car._source.modelo}
+              // carYear={car._source.año}
+              // carLocation={car._source.estado_agencia} //{car.carLocation}
+              // carAgency={car._source.municipio_agencia}
+              // carColor={car._source.colores.length}
+              // carPrice={car._source.precio}
+              {...cardProps[cardType]}
+            />
+          </Grid>
+      );
+    }
+  });
   }
   else {
     carList = <h1 className="d-flex flex-column justify-content-center align-items-center"> No se encontraron autos </h1>
