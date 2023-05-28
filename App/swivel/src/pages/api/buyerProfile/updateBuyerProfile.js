c5b39821226e1fa0bf42bfe6288720c87ff882b3
Andreina Sananez <asananez2000@gmail.com>
2023-05-28 14:13:32 -0600

    Add API to edit buyer profile

---
+++ App/swivel/src/pages/api/buyerProfile/updateBuyerProfile.js
@@ -0,0 +1,25 @@
+
+import { User, BuyerUser } from "../../models/user";
+import dbConnect from "../../config/dbConnect";
+
+export default async function handler(req, res) {
+  if (req.method === "PUT") {
+    dbConnect();
+
+    const { id, name, surname, email, phone, address } = req.body;
+
+    await BuyerUser.updateOne(
+        { _id: id },
+        { $set: { nombres: name, 
+                  apellidos: surname, 
+                  email: email, 
+                  numero_telefonico: phone, 
+                  address: address } 
+        }
+
+    );
+
+    res.status(200).json({ message: "profile info updated correctly" });
+
+  }
+}
