0d3da93b7f180eb54269e905301627856d45205f
ivalani amador <ivalanipiaga@gmail.com>
2023-05-29 00:26:16 -0600

    starting landing page

--- App/swivel/src/pages/providers/GA.js
+++ App/swivel/src/pages/providers/GA.js
@@ -1,7 +1,12 @@
+import React from "react";
+import CustomHero from "@/components/general/custom_hero";
 import GANavbar from '@/components/providers/GA/navbar'
-import { useRouter } from 'next/router'
+import GActionsCards from "@/components/providers/GA/ga_actions_cards";
+//import { useRouter } from 'next/router'
 
 export default function GALandingPage() {
+
+  /*
   const router = useRouter();
 
   const routToCatalogue = () => {
@@ -16,11 +21,28 @@ export default function GALandingPage() {
   const routToStatistics = () => {
     router.push('/providers/new_GA/'); // <-- add your route
   };
-
+*/
 
   return (
     <>
       <GANavbar/>
+      <CustomHero
+        title="Bienvenidx, nombre"
+        message="Administra tus solicitudes pendientes"
+      />
+      <GActionsCards/>
+
+    </>
+  )
+  /*
+  return (
+    <>
+      <GANavbar/>
+      <CustomHero
+        title="Bienvenidx, nombre"
+        message="Administra tus solicitudes pendientes"
+      />
+      <SellerRequestCards/>
 
       <div>
         <button onClick={routToCatalogue}>
@@ -38,4 +60,5 @@ export default function GALandingPage() {
       </div>
     </>
   )
+  */
 }
