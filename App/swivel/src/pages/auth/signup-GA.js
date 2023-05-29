ced6bd6abb4c4aa67370fd221c84aff80c48427e
ivalani amador <ivalanipiaga@gmail.com>
2023-05-29 01:17:47 -0600

    st adapt signup with the component

--- App/swivel/src/pages/auth/signup-GA.js
+++ App/swivel/src/pages/auth/signup-GA.js
@@ -3,6 +3,8 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import AuthComponent from "@/components/login/auth_component";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Typography from "@mui/material/Typography";
 
 /* Función que retorna el formulario de registro de GA con su dirección, junto con los botones de ingreso  */
 export default function SignupBuyerData() {
@@ -27,6 +29,8 @@ export default function SignupBuyerData() {
     const [legalSurname, setLegalSurname] = useState("");
     const [legalEmail, setLegalEmail] = useState("");
     const [legalPhone, setLegalPhone] = useState("");
+
+    const [activeSectionIndex, setActiveSectionIndex] = useState(0);
 
     const [first, setFirst] = useState(true);
     const [second, setSecond] = useState(false);
@@ -72,7 +76,184 @@ export default function SignupBuyerData() {
             console.log(error.response.data);
         }
     };
+    return (
+        <>
+            {activeSectionIndex === 0 && (
+                <>
+                    <AuthComponent
+                        title="Crea tu cuenta"
+                        fields={
+                            <form className="d-flex flex-column" onSubmit={() => { setActiveSectionIndex(1) }}>
+                                <div className="form-outline mb-2">
+                                    <div className="d-flex flex-row ">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Nombre(s)"
+                                            value={name}
+                                            pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
+                                            onChange={(e) => setName(e.target.value)}
+                                            required
+                                        />
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Apellidos"
+                                            value={surname}
+                                            pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
+                                            onChange={(e) => setSurname(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-outline mb-2">
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        placeholder="Correo Electrónico"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-outline mb-2">
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        placeholder="Contraseña"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-outline mb-2">
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        placeholder="Confirmar Contraseña"
+                                        required
+                                    />
+                                </div>
+                                <div className="d-flex flex-column text-center pt-1 mb-2 pb-1">
+                                    <button className="btn btn-primary btn-block mb-2" onSubmit={submitHandler}>
+                                        <Typography
+                                            wrap sx={{
+                                                color: "white",
+                                                fontFamily: "lato",
+                                            }}
+                                        >
+                                            {" "}
+                                            Continuar el Registro{" "}
+                                        </Typography>
+                                    </button>
+                                </div>
+                                <div className="text-center">
+                                    <p>
+                                        ¿Ya tienes una cuenta?<a href="#!"> Regístrate aquí</a> 
+                                    </p>
+                                </div>
+                            </form>
+                        }
+                    />
+                </>
+            )}
 
+            {activeSectionIndex === 1 && (
+                <>
+                    <AuthComponent
+                        title="Información del grupo"
+                        fields={
+                            <form className="d-flex flex-column" onSubmit={submitHandler}>
+                                <div className="form-outline mb-2">
+                                    <div className="d-flex flex-row ">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Nombre del GA"
+                                            value={agencyName}
+                                            onChange={(e) => setAgencyName(e.target.value)}
+                                            required
+                                        />
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="RFC"
+                                            value={rfc}
+                                            onChange={(e) => setRfc(e.target.value)}
+                                            required
+                                        />
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Calle"
+                                            value={street}
+                                            onChange={(e) => setStreet(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-outline mb-2">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Numero interior"
+                                        value={interior_num}
+                                        onChange={(e) => setInteriorNum(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-outline mb-2">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Ciudad"
+                                        value={city}
+                                        onChange={(e) => setCity(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-outline mb-2">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Estado"
+                                        value={state}
+                                        onChange={(e) => setState(e.target.value)}
+                                        required
+                                    />
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="País"
+                                        value={country}
+                                        onChange={(e) => setCountry(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="d-flex flex-column text-center pt-1 mb-2 pb-1">
+                                    <button className="btn btn-primary btn-block mb-2" onSubmit={() => setActiveSectionIndex(2)}>
+                                        <Typography
+                                            wrap sx={{ color: "white", fontFamily: "lato" }}>
+                                            {" "}
+                                            Crear Cuenta{" "}
+                                        </Typography>
+                                    </button>
+                                    <button type="submit" className="btn btn-secondary btn-block mb-2">
+                                        <Typography
+                                            sx={{ color: "white", fontFamily: "lato" }}>
+                                            {" "}
+                                            Cancelar{" "}
+                                        </Typography>
+                                    </button>
+                                </div>
+                            </form>
+                        }
+                    />
+                </>
+            )}
+        </>
+    );
+/*
     useEffect(() => {
     }, [first, second, third]);
 
@@ -93,8 +274,12 @@ export default function SignupBuyerData() {
         setSecond(false);
         setThird(true);
     };
+*/
 
+
+    /*
     return (
+        
         <div> 
             {first && <div>
                 <input
@@ -303,5 +488,5 @@ export default function SignupBuyerData() {
                 </button>
             </div>}
         </div>
-    );
+    );*/
 }
