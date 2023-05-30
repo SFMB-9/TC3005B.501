1d19e07b6dffc7730575871929f8cdfa559728d1
Ace0x <59582360+Ace0x@users.noreply.github.com>
2023-05-29 21:52:41 -0500

    finished documentosGA

---
+++ App/swivel/src/pages/api/GA/uploadDocsGA.js
@@ -0,0 +1,32 @@
+import dbConnect from "../../../config/dbConnect";
+const Proceso = require('../../../models/procesos');
+
+
+export default async (req, res) => {
+   
+    const id = req.body.id;
+    const documentos = req.body.documentos;
+
+    if(req.method !== 'PUT'){
+        return res.status(405).json({message: 'Metodo no permitido'})
+    }
+
+    // Connect to the MongoDB database using Mongoose
+    await dbConnect();
+
+    try {
+        const docs = Proceso.findById(id);
+        if (!docs) {
+            return res.status(404).json({ message: 'No se encontro el usuario' });
+        }
+        
+        docs.documentos = documentos;
+        docs.save();
+
+        return res.status(200).json({ message: 'Documentos actualizados' });
+
+    } catch (error) {
+        return res.status(500).json({ message: 'Error al actualizar los documentos' });
+    }
+
+};
--- App/swivel/src/pages/providers/GA/documentosGA.js
+++ App/swivel/src/pages/providers/GA/documentosGA.js
@@ -1,17 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from 'next/router';
+import FileUpload from "@pages/api/uploadBucketDoc/uploadBucketDoc"
 
-const registroAgencia = () => {
-    
-    const docList = {
-        "lisencia": "",
-        "id": "",
+const RegistroAgencia = () => {
+  const [docs, setDocs] = useState(null);
+  const router = useRouter();
+  const { _id } = router.query;
+  const [files, setFiles] = useState([]);
+
+  const getDocs = async () => {
+    const response = await axios.get("http://localhost:3000/api/DrivingRequest/getDrivingRequest", {
+      params: { _id: _id }
+    });
+    setDocs(response.data);
+  };
+
+  const uploadDocs = async () => {
+    const response = await axios.put("http://localhost:3000/api/GA/uploadDocsGA", {
+      id: _id,
+      documentos: docs
+    });
+    console.log(response.data);
+  };
+
+  const handleFileChange = async(event, index) => {
+    const uploadedFile = event.target.files[0];
+    const updatedFiles = [...files];
+    updatedFiles[index] = uploadedFile;
+    setFiles(updatedFiles);
+    for (let i = 0; i < files.length; i++){
+        const url = await FileUpload(files[i]);
+        const updatedDocs = docs;
+        updatedDocs.documentos[i].url = url;
+        setDocs(updatedDocs);
     }
-    const [docs, setDocs] = useState();
+    uploadDocs();
+  };
+
+  useEffect(() => {
+    getDocs();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Fecha Actualización</th>
+          <th>Estatus</th>
+          <th>Comentarios</th>
+          <th>File Upload</th>
+        </tr>
+      </thead>
+      <tbody>
+        {docs.documentos.map((item, index) => (
+          <tr key={index}>
+            <td>{item.nombre}</td>
+            <td>{item.fechaActualizacion}</td>
+            <td>{item.estatus}</td>
+            <td>{item.comentarios}</td>
+            <td>
+              <input type="file" onChange={(event) => handleFileChange(event, index)} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default RegistroAgencia;
 
     
-    
 
 
-}
