e32c9a9cf26b54251751ee561ceab3602be59e2f
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-06-01 15:00:08 -0600

    added redirection

--- App/swivel/src/pages/pruebademanejo/[process_id].js
+++ App/swivel/src/pages/pruebademanejo/[process_id].js
@@ -342,7 +342,7 @@ export default function Process() {
             }}
             disableElevation
             type="button"
-            href="/"
+            href="/account/tests"
           >
             Ok
           </Button>
