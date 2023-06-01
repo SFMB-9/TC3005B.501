54aec9687a881e3fbdb38bb81d02cbe400a38e0b
ivalani amador <ivalanipiaga@gmail.com>
2023-05-31 18:34:21 -0600

    right actions nav

--- App/swivel/src/components/providers/GA/navbar.jsx
+++ App/swivel/src/components/providers/GA/navbar.jsx
@@ -5,7 +5,7 @@ export default function GANavbar() {
     <CustomNavbar
       home='/providers/GA'
       elems_right={[
-        {name: 'Acciones', href: '', popup: [{name: 'Revisión de solicitudes', href: ''}, {name: 'Administración de S.A', href: ''}, {name: 'Administración de la grupos', href: ''}, {name: 'Estadísticas de ventas', href: ''}]},
+        {name: 'Acciones', href: '', popup: [{name: 'Manejo de catálogo', href: ''}, {name: 'Administración de gerentes', href: ''}, {name: 'Administración de la agencias', href: ''}, {name: 'Estadísticas de ventas', href: ''}]},
         {name: 'Mi cuenta', href: '', popup: [{name: 'Nombre del usuario', href: ''}, {name: 'Cambiar contraseña', href: '/providers/GA-update-password'}, {name: 'Cerrar sesión', href:'#', signoutComponent: '/auth/login'}]},
       ]}
     />
