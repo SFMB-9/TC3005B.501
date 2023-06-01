04f51e54e3d86349baae4807c90ffd5154ad0dd0
Do Hyun Nam <dollis2002@gmail.com>
2023-05-31 20:15:42 -0600

    Added correct documentation

--- App/swivel/src/pages/api/GA/create-agencia.js
+++ App/swivel/src/pages/api/GA/create-agencia.js
@@ -1,7 +1,48 @@
 import { AgencyEntity } from "@/models/user";
 import dbConnect from "@/config/dbConnect";
 
-// Creates new agency
+/* 
+Creates agency
+@Recieves: request object, response object
+    Recieves following fields in request body:
+    {
+        is_account_verified: Boolean,
+        documentos_requeridos_compra: Array,
+        horas_min: Number,
+        dias_anticipo: Number,
+        dias_max: Number,
+        grupo_automotriz_id: String,
+        horas_max: Number,
+
+        //registro-direccion
+        direccion: {
+        calle: String,
+        numero_exterior: String,
+            numero_interior: String,
+            ciudad: String,
+            estado: String,
+            pais: String,
+            codigo_postal: String,
+        },
+    
+        url_agencia: String,
+        coordenadas_agencia: {
+            location: {
+                type: {
+                    type: String, // Don't do `{ location: { type: String } }`
+                    enum: ['Point'], // 'location.type' must be 'Point'
+                    required: true
+                },
+                coordinates: {
+                    type: [Number],
+                    required: true
+                }
+            }
+        },
+    }
+@Returns: response status and json
+*/
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     dbConnect();
@@ -14,7 +55,9 @@ export default async function handler(req, res) {
       if (err) {
         res.status(500).json({ message: "Error al crear la agencia" });
       } else {
-        res.status(200).json({ message: "Agencia creada exitosamente", agency: agency });
+        res
+          .status(200)
+          .json({ message: "Agencia creada exitosamente", agency: agency });
       }
     });
   } else {
