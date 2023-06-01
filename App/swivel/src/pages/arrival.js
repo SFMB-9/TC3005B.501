72d83bf282555e69621cd4c30b1ff28ed456fe29
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2023-05-31 23:23:05 -0600

    Create arrival page and signin wall to tests

---
+++ App/swivel/src/pages/arrival.js
@@ -0,0 +1,33 @@
+import CustomHero from "@/components/general/custom_hero";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+
+export default function arrival() {
+  const [apiData, setApiData] = useState(null);
+
+  const { data: session } = useSession();
+
+  const fetchData = async () => {
+    const resData = await fetch(
+      `/api/managerProfile/managerP?id=${session.id}`
+    );
+
+    const res = await resData.json();
+
+    setApiData(res.userData);
+  };
+
+  useEffect(() => {
+    if (session) {
+      fetchData();
+    }
+  }, [session]);
+
+  return(
+    <CustomHero
+      title={` Bienvenidx, ${apiData.nombres}`}
+      message={''}
+      containerSize={100}
+    />
+  )
+}
