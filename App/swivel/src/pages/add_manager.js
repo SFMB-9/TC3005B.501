1109366a53e64c315dfa2e21ab64437c2974b87a
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-05-31 19:38:28 -0600

    Finished component

--- App/swivel/src/pages/add_manager.js
+++ App/swivel/src/pages/add_manager.js
@@ -79,36 +79,40 @@ export default function AddManager() {
         <div>
             <BuyerNavbar />
             <div className={styles.mainContainer}>
-                <h1 className={styles.pageTitle}>Ingresa los datos del gerente</h1>
+                <h1 className={styles.pageTitle}>Agregar Gerente</h1>
+                <h3 className={styles.boldText}>Ingresa los datos del gerente</h3>
                 <form>
                     <div className={styles.inputContainer}>
-                        <h5>Nombre(s)</h5>
-                        <TextField
-                            className={styles.inputFieldLeft}
-                            id="nombres"
-                            label="Nombre(s)"
-                            variant="outlined"
-                            fullWidth
-                            value={formValues.nombres}
-                            onChange={handleChange}
-                            error={!!errors.nombres}
-                            helperText={errors.nombres}
-                        />
-                    </div>
-
-                    <div className={styles.inputContainer}>
-                        <h5>Apellido(s)</h5>
-                        <TextField
-                            className={styles.inputField}
-                            id="apellidos"
-                            label="Apellido(s)"
-                            variant="outlined"
-                            fullWidth
-                            value={formValues.apellidos}
-                            onChange={handleChange}
-                            error={!!errors.apellidos}
-                            helperText={errors.apellidos}
-                        />
+                        <div className={styles.row}>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Nombre(s)</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    id="nombres"
+                                    label="Nombre(s)"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={formValues.nombres}
+                                    onChange={handleChange}
+                                    error={!!errors.nombres}
+                                    helperText={errors.nombres}
+                                />
+                            </div>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Apellido(s)</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    id="apellidos"
+                                    label="Apellido(s)"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={formValues.apellidos}
+                                    onChange={handleChange}
+                                    error={!!errors.apellidos}
+                                    helperText={errors.apellidos}
+                                />
+                            </div>
+                        </div>
                     </div>
 
                     <div className={styles.inputContainer}>
@@ -141,7 +145,7 @@ export default function AddManager() {
                         />
                     </div>
 
-                    <h1 className={styles.pageTitle}>Credenciales</h1>
+                    <h3 className={styles.boldText}>Credenciales</h3>
                     <div className={styles.inputContainer}>
                         <h5>Correo Electrónico</h5>
                         <TextField
@@ -158,35 +162,38 @@ export default function AddManager() {
                     </div>
 
                     <div className={styles.inputContainer}>
-                        <h5>Contraseña</h5>
-                        <TextField
-                            className={styles.longInputField}
-                            id="contraseña"
-                            label="Contraseña"
-                            variant="outlined"
-                            fullWidth
-                            value={formValues.contraseña}
-                            onChange={handleChange}
-                            error={!!errors.contraseña}
-                            helperText={errors.contraseña}
-                            type="password"
-                        />
-                    </div>
-
-                    <div className={styles.inputContainer}>
-                        <h5>Confirmar Contraseña</h5>
-                        <TextField
-                            className={styles.longInputField}
-                            id="confirmarContraseña"
-                            label="Confirmar Contraseña"
-                            variant="outlined"
-                            fullWidth
-                            value={formValues.confirmarContraseña}
-                            onChange={handleChange}
-                            error={!!errors.confirmarContraseña}
-                            helperText={errors.confirmarContraseña}
-                            type="password"
-                        />
+                        <div className={styles.row}>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Contraseña</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    id="contraseña"
+                                    label="Contraseña"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={formValues.contraseña}
+                                    onChange={handleChange}
+                                    error={!!errors.contraseña}
+                                    helperText={errors.contraseña}
+                                    type="password"
+                                />
+                            </div>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Confirmar Contraseña</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    id="confirmarContraseña"
+                                    label="Confirmar Contraseña"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={formValues.confirmarContraseña}
+                                    onChange={handleChange}
+                                    error={!!errors.confirmarContraseña}
+                                    helperText={errors.confirmarContraseña}
+                                    type="password"
+                                />
+                            </div>
+                        </div>
                     </div>
 
                     <div className={styles.buttonContainer}>
