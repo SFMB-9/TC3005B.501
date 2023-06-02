df0cda346a962970cb6ab825ff91cbb1fb933ee1
ivalani amador <ivalanipiaga@gmail.com>
2023-06-02 01:54:23 -0600

    add navbar integration

--- App/swivel/src/components/providers/GA/navbar.jsx
+++ App/swivel/src/components/providers/GA/navbar.jsx
@@ -4,6 +4,9 @@ export default function GANavbar() {
   return (
     <CustomNavbar
       home='/providers/GA'
+      elems_right={[
+        {name: 'Mi cuenta', href: '', popup: [{name: 'Nombre del usuario', href: ''}, {name: 'Cambiar contraseña', href: '/providers/GA-update-password'}, {name: 'Cerrar sesión', href:'#', signoutComponent: '/auth/login'}]},
+      ]}
     />
   )
 }
