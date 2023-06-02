c6b08bdd8bc371e9750ef98c831ffc73771950ba
ivalani amador <ivalanipiaga@gmail.com>
2023-06-02 02:07:34 -0600

    change password int GA

--- App/swivel/src/pages/providers/GA-update-password.js
+++ App/swivel/src/pages/providers/GA-update-password.js
@@ -1,4 +1,5 @@
 import GANavbar from "@/components/providers/GA/navbar"
+import ChangePassword from "@/components/login/change_password"
 import axios from "axios"
 import { useState } from "react"
 //import handler from '../api/GA/GA-change-password'
@@ -27,22 +28,7 @@ export default function GAUpdatePassword() {
   return (
     <>
     <GANavbar/>
-    <div>
-      <h1>Cambiar contraseña</h1>
-      <p>Se cerrarán todas las sesiones, excepto a la actual, para proteger tu cuenta</p>
-      <p>La contraseña debe tener al menos seis carácteres, e incluir una combinación de números, letras y carácteres especiales. (!$@%)</p>
-      <form onSubmit={submitHandler}>
-        <input name="oldPassword" value={oldPassword} placeholder="Contraseña" onChange={(e) => setOldPassword(e.target.value)}/>
-        <input name="password" value={newPassword} placeholder="Contraseña" onChange={(e) => setNewPassword(e.target.value)}/>
-        <input name="passwordConfirm" value={passwordConfirmation} placeholder="Contraseña" onChange={(e) => setPasswordConfirmation(e.target.value)}/>
-        <button onClick={() => {
-          setOldPassword('')
-          setNewPassword('')
-          setPasswordConfirmation('')
-        }}>Cancelar</button>
-        <button type="submit">Guardar</button>
-      </form>
-    </div>
+    <ChangePassword/>
     </>
   )
 }
