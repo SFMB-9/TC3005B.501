90e8e7cff4433813b3dd0436ee3663d57c6d80bf
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-06-02 01:41:59 -0600

    Integrated signup, detalle-agencia, and editProfile

--- App/swivel/src/pages/providers/GA/editProfile.js
+++ App/swivel/src/pages/providers/GA/editProfile.js
@@ -1,71 +1,166 @@
 import React, { useState, useEffect } from 'react';
+import { Button, TextField } from '@mui/material';
+import styles from '@/styles/edit_GA.module.css';
 import axios from 'axios';
-import { useRouter } from "next/router";
+import GANavbar from '@/components/providers/GA/navbar';
+import { useRouter } from 'next/router';
 
-export default function SearchResults() {
-  const router = useRouter();
-  const id = "6477e14bae27e558e56c3c13";
+export default function EditGA() {
+    const router = useRouter();
+    const id = "6477e14bae27e558e56c3c13";
 
-  const [name, setName] = useState('');
-  const [surname, setSurname] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
 
-  // Function to fetch search results from the API endpoint
-  const fetchResults = async () => {
-    try {
-      const response = await axios.get("/api/managerProfile/managerP", { params: { id: id }});
-      console.log(response.data.userData);
-      const userData = response.data.userData;
-      setName(userData.nombres || ''); // Provide an empty string as the initial value
-      setSurname(userData.surname || ''); // Provide an empty string as the initial value
-      setEmail(userData.email || ''); // Provide an empty string as the initial value
-      setPhone(userData.numero_telefonico || ''); // Provide an empty string as the initial value
-    } 
-    catch (error) {
-      console.error('Error fetching search results:', error);
-    }
-  };
+    // Function to fetch search results from the API endpoint
+    const fetchResults = async () => {
+        try {
+            const response = await axios.get("/api/managerProfile/managerP", { params: { id: id } });
+            console.log(response.data.userData);
+            const userData = response.data.userData;
+            setName(userData.nombres || ''); // Provide an empty string as the initial value
+            setSurname(userData.surname || ''); // Provide an empty string as the initial value
+            setEmail(userData.email || ''); // Provide an empty string as the initial value
+            setPhone(userData.numero_telefonico || ''); // Provide an empty string as the initial value
+        }
+        catch (error) {
+            console.error('Error fetching search results:', error);
+        }
+    };
 
-  // Fetch results when the component mounts
-  useEffect(() => {
-    fetchResults();
-  }, []);
+    // Fetch results when the component mounts
+    useEffect(() => {
+        fetchResults();
+    }, []);
 
-  const submitHandler = async () => {
-    await axios.put('/api/GA/editGA', { id, name, surname, email, phone });
-  };
+    const submitHandler = async () => {
+        await axios.put('/api/GA/editGA', { id, name, surname, email, phone });
+    };
 
-  const cancelHandler = () => {
-    router.push(`/providers/GA`);
-  };
+    const cancelHandler = () => {
+        router.push(`/providers/GA`);
+    };
 
-  return (
-    <>
-      <h1>Detalles del Gerente</h1>
-      <label>
-        Nombre:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-      </label>
-      <label>
-        Apellidos:
-        <input type="text" value={surname} onChange={(e) => setSurname(e.target.value)}/>
-      </label>
-      <label>
-        Email:
-        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-      </label>
-      <label>
-        Teléfono:
-        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)}/>
-      </label>
 
-      <button onClick={cancelHandler}>
-        Cancelar
-      </button>
-      <button onClick={submitHandler}>
-        Guardar
-      </button>
-    </>
-  );
+
+
+    //Old code
+    // const [formValues, setFormValues] = useState({
+    //     nombre: '',
+    //     apellido: '',
+    //     correo: '',
+    //     telefono: '',
+    // });
+
+    // const handleChange = (event) => {
+    //     const { id, value } = event.target;
+    //     setFormValues((prevValues) => ({
+    //         ...prevValues,
+    //         [id]: value,
+    //     }));
+    // };
+
+    // const handleSave = (event) => {
+    //     event.preventDefault();
+    //     console.log('Form values:', formValues);
+    //     // Upload form data
+    // };
+
+    // const handleCancel = () => {
+    //     // Handle cancel logic here
+    //     console.log('Cancel');
+    // };
+
+    return (
+        <div>
+            <GANavbar />
+            <div className={styles.mainContainer}>
+                <h1 className={styles.pageTitle}>Editar Perfil</h1>
+                <div className={styles.row}>
+                    <div className={styles.inputContainer}>
+                        <h5>Nombre(s)</h5>
+                        <TextField
+                            className={styles.inputFieldLeft}
+                            id="nombre"
+                            label={name}
+                            variant="outlined"
+                            fullWidth
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+
+                    <div className={styles.inputContainer}>
+                        <h5>Apellido(s)</h5>
+                        <TextField
+                            className={styles.inputField}
+                            id="apellido"
+                            label={surname}
+                            variant="outlined"
+                            fullWidth
+                            onChange={(e) => setSurname(e.target.value)}
+                        />
+                    </div>
+                </div>
+
+                <div className={styles.inputContainer}>
+                    <h5>Correo</h5>
+                    <TextField
+                        className={styles.longInputField}
+                        id="correo"
+                        label={email}
+                        variant="outlined"
+                        fullWidth
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div className={styles.inputContainer}>
+                    <h5>Teléfono</h5>
+                    <TextField
+                        className={styles.longInputField}
+                        id="telefono"
+                        label={phone}
+                        variant="outlined"
+                        fullWidth
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                </div>
+
+                <div className={styles.buttonContainer}>
+                    <Button
+                        variant="contained"
+                        className={styles.button}
+                        disableElevation
+                        sx={{
+                            backgroundColor: '#979797',
+                            fontFamily: 'lato',
+                            fontWeight: 'bold',
+                            ':hover': { backgroundColor: '#BABABA' },
+                        }}
+                        onClick={cancelHandler}
+                    >
+                        Cancelar
+                    </Button>
+
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        className={styles.button}
+                        disableElevation
+                        onClick={submitHandler}
+                        sx={{
+                            backgroundColor: '#F55C7A',
+                            fontFamily: 'lato',
+                            fontWeight: 'bold',
+                            ':hover': { backgroundColor: '#BABABA' },
+                        }}
+                    >
+                        Guardar
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
--- App/swivel/src/pages/providers/manager/signup.js
+++ App/swivel/src/pages/providers/manager/signup.js
@@ -1,111 +1,228 @@
-
-"use client";
-
 import axios from "axios";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
+import { Button, TextField } from "@mui/material";
+import styles from "@/styles/add_manager.module.css";
+import GANavbar from "@/components/providers/GA/navbar";
 
-export default function ManagerSignup() {
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
+export default function MergedSignup() {
+    const [formValues, setFormValues] = useState({
+        nombres: "",
+        apellidos: "",
+        telefono: "",
+        agencia: "",
+        correo: "",
+        contraseña: "",
+        confirmarContraseña: "",
+    });
 
-  const router = useRouter();  
+    const [errors, setErrors] = useState({
+        nombres: "",
+        apellidos: "",
+        telefono: "",
+        agencia: "",
+        correo: "",
+        contraseña: "",
+        confirmarContraseña: "",
+    });
 
-  const GA = router.query.GA_id;	
-  const agency = router.query.agency_id;
+    const router = useRouter();
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setFormValues((prevValues) => ({
+            ...prevValues,
+            [id]: value,
+        }));
+    };
 
-    try {
-      const { data } = await axios.post("/api/register", {
-        nombres: name,
-        apellidos: surname,
-        email: email,
-        password: password,
-        tipo_usuario: "manager",
-        agencia_id: agency,
-        grupo_automotriz_id: GA,
-        numero_telefonico: phone,
-      });
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        let isValid = true;
+        const newErrors = { ...errors };
 
-      console.log(data);
-    } catch (error) {
-      console.log(error.response.data);
-    }
-  };
+        const router = useRouter();
 
-  return (
-    <>
-      <form onSubmit={submitHandler}>
-        <h1>Manager Register</h1>
+        const GA = router.query.GA_id;
+        const agency = router.query.agency_id;
 
+        // Check if any of the fields are empty
+        for (const key in formValues) {
+            if (formValues[key] === "") {
+                newErrors[key] = "This field is required";
+                isValid = false;
+            } else {
+                newErrors[key] = "";
+            }
+        }
+
+        // Check if the email is valid
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(formValues.correo)) {
+            newErrors.correo = "Please enter a valid email";
+            isValid = false;
+        }
+
+        // Check if the passwords match
+        if (formValues.contraseña !== formValues.confirmarContraseña) {
+            newErrors.confirmarContraseña = "Passwords do not match";
+            isValid = false;
+        }
+
+        if (!isValid) {
+            setErrors(newErrors);
+            return; // Exit the function if any validation fails
+        }
+
+        try {
+            const { data } = await axios.post("/api/register", {
+                nombres: formValues.nombres,
+                apellidos: formValues.apellidos,
+                email: formValues.correo,
+                password: formValues.contraseña,
+                tipo_usuario: "manager",
+                agencia_id: agency,
+                grupo_automotriz_id: GA,
+                numero_telefonico: phone,
+            });
+
+            console.log(data);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
+    const handleCancel = () => {
+        // Handle cancel logic here
+        console.log("Cancel");
+    };
+
+    return (
         <div>
-          <label htmlFor="name_field">Name</label>
-          <input
-            type="text"
-            id="name_field"
-            className="form-control"
-            value={name}
-            pattern="[a-zA-Z]+"
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
+            <GANavbar />
+            <div className={styles.mainContainer}>
+                <h1 className={styles.pageTitle}>Agregar Gerente</h1>
+                <h3 className={styles.boldText}>Ingresa los datos del gerente</h3>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.inputContainer}>
+                        <div className={styles.row}>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Nombre(s)</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    id="nombres"
+                                    label="Nombre(s)"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={formValues.nombres}
+                                    onChange={handleChange}
+                                    error={!!errors.nombres}
+                                    helperText={errors.nombres}
+                                />
+                            </div>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Apellido(s)</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    id="apellidos"
+                                    label="Apellido(s)"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={formValues.apellidos}
+                                    onChange={handleChange}
+                                    error={!!errors.apellidos}
+                                    helperText={errors.apellidos}
+                                />
+                            </div>
+                        </div>
+                    </div>
 
-        <div>
-          <label htmlFor="name_field">Surname</label>
-          <input
-            type="text"
-            id="name_field"
-            className="form-control"
-            value={surname}
-            pattern="[a-zA-Z]+"
-            onChange={(e) => setSurname(e.target.value)}
-            required
-          />
-        </div>
+                    <div className={styles.inputContainer}>
+                        <h5>Teléfono</h5>
+                        <TextField
+                            className={styles.longInputField}
+                            id="telefono"
+                            label="Teléfono"
+                            variant="outlined"
+                            fullWidth
+                            value={formValues.telefono}
+                            onChange={handleChange}
+                            error={!!errors.telefono}
+                            helperText={errors.telefono}
+                        />
+                    </div>
 
-        <div>
-          <label htmlFor="email_field">Email address</label>
-          <input
-            type="email"
-            id="email_field"
-            className="form-control"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
+                    <h3 className={styles.boldText}>Credenciales</h3>
+                    <div className={styles.inputContainer}>
+                        <h5>Correo Electrónico</h5>
+                        <TextField
+                            className={styles.longInputField}
+                            id="correo"
+                            label="Correo Electrónico"
+                            variant="outlined"
+                            fullWidth
+                            value={formValues.correo}
+                            onChange={handleChange}
+                            error={!!errors.correo}
+                            helperText={errors.correo}
+                        />
+                    </div>
 
-        <div>
-          <label htmlFor="phone_field">Phone Number</label>
-          <input
-            type="text"
-            id="phone_field"
-            className="form-control"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            required
-          />
-        </div>
+                    <div className={styles.inputContainer}>
+                        <div className={styles.row}>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Contraseña</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    id="contraseña"
+                                    label="Contraseña"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={formValues.contraseña}
+                                    onChange={handleChange}
+                                    error={!!errors.contraseña}
+                                    helperText={errors.contraseña}
+                                    type="password"
+                                />
+                            </div>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Confirmar Contraseña</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    id="confirmarContraseña"
+                                    label="Confirmar Contraseña"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={formValues.confirmarContraseña}
+                                    onChange={handleChange}
+                                    error={!!errors.confirmarContraseña}
+                                    helperText={errors.confirmarContraseña}
+                                    type="password"
+                                />
+                            </div>
+                        </div>
+                    </div>
 
-        <div>
-          <label htmlFor="password_field">Password</label>
-          <input
-            type="password"
-            id="password_field"
-            className="form-control"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+                    <div className={styles.buttonContainer}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            className={styles.button}
+                        >
+                            Register
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            onClick={handleCancel}
+                            className={styles.button}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <button type="submit">Register</button>
-      </form>
-    </>
-  );
+    );
 }
