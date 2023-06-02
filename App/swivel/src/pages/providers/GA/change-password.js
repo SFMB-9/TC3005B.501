c2e025465fc7510dd9920f87e74c9e5e882dbb1b
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-06-02 08:48:24 -0600

    Create change-password.js

---
+++ App/swivel/src/pages/providers/GA/change-password.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChangePassword from '@/components/login/change_password'
+import SANavbar from '@/components/SA/navbar'
+
+export default function changePassword() {
+    return (
+        <div>
+            <SANavbar />
+            <ChangePassword />
+        </div>
+    )
+}
