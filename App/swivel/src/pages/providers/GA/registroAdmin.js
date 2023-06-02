c5c57e6cbacc09f1a8ac3fd2366290df7bf13eda
Ace0x <59582360+Ace0x@users.noreply.github.com>
2023-06-02 10:19:16 -0700

    fixed register

--- App/swivel/src/pages/providers/GA/registroAdmin.js
+++ App/swivel/src/pages/providers/GA/registroAdmin.js
@@ -42,6 +42,7 @@ const registroAdmin = () => {
                 email: email,
                 password: password,
                 numero_telefonico: phone,
+                grupo_automotriz_id: GA._id
             });
             console.log(response.data);
         } catch (error) {
