fc55c3e88b60c9b199679ca6314d016a95aca17d
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-06-02 11:17:01 -0600

    Fix to SA password

--- App/swivel/src/pages/superadmin/change-passwordSA.js
+++ App/swivel/src/pages/superadmin/change-passwordSA.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ChangePassword from '@/components/login/change_password'
+import ChangePassword from '@/components/SA/change_password'
 import SANavbar from '@/components/SA/navbar'
 
 export default function changePasswordSA() {
