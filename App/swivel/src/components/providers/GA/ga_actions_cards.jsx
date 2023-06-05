85a1583dce5572e9eb4f168c03be110c147dd912
karla-mond <karla.mondragon.rosas@gmail.com>
2023-06-05 17:16:49 -0600

    re route admin de GA y agencias

--- App/swivel/src/components/providers/GA/ga_actions_cards.jsx
+++ App/swivel/src/components/providers/GA/ga_actions_cards.jsx
@@ -7,8 +7,8 @@ export default function GActionsCards() {
       <CustomIconCard
         imageSource="/manager/seller_action.jpg"
         icon="/manager/seller_icon.svg"
-        text="Administración de gerentes"
-        href="/providers/GA/manageGA" //<-- there are still no route to add
+        text="Administración de Agencias"
+        href="/providers/GA/portal_agencias"
         light={false}
         header=''
         fiftyFifty={false}
@@ -16,8 +16,8 @@ export default function GActionsCards() {
       <CustomIconCard
         imageSource="/manager/branch_action.jpg"
         icon="/manager/branch_icon.svg"
-        text="Administración de agencias"
-        href="/providers/GA/portal_agencias" //<-- there are still no route to add
+        text="Administración de Grupo Automotriz"
+        href="/providers/GA/manageGA" //<-- there are still no route to add
         light={false}
         header=''
         fiftyFifty={false}
