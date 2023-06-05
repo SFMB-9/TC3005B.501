d65d211b0fa649715c6f9213ebf8172ea39beffd
SeisshinKB <75494927+ivalani@users.noreply.github.com>
2023-06-04 23:13:24 -0600

    signup update

--- App/swivel/src/pages/auth/signup-GA.js
+++ App/swivel/src/pages/auth/signup-GA.js
@@ -14,6 +14,8 @@ export default function SignupGAData() {
     const [surname, setSurname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confPassword, setConfPassword] = useState("");
+    const [confPasswordHelper, setConfPasswordHelper] = useState("");
 
     const [agencyName, setAgencyName] = useState("");
     const [url, setUrl] = useState("");
@@ -33,24 +35,41 @@ export default function SignupGAData() {
     const [legalPhone, setLegalPhone] = useState("");
     const { encryptRole } = require("@/utils/crypto");
 
-
+    const [errors, setErrors] = useState({
+        name: false,
+        surname: false,
+        email: false,
+        password: false,
+        confPassword: false,
+        phone: false,
+      });
+      const [error, setError] = useState(false);
+      const [loading, setLoading] = useState(false);
+    
+      const disabled = () => {
+        for (const key in errors) {
+          if (errors[key]) return true;
+        }
+        return false;
+      };
+    
+      const [errMessage, setErrMessage] = useState("");
+      let passStatus = null;
+    
+    
     const [activeSectionIndex, setActiveSectionIndex] = useState(0);
-    /*
-    const [first, setFirst] = useState(true);
-    const [second, setSecond] = useState(false);
-    const [third, setThird] = useState(false);*/
 
     const submitHandler = async (e) => {
         e.preventDefault();
 
         try {
-            const { data } = await axios.post("/api/GA/GA-register", {
-                tipo_usuario: 'GA',
+            const { data } = await axios.post("/api/register", {
+                tipo_usuario: 'user',
                 tipo_entidad: 'GAEntity',
                 nombre_agencia: agencyName,
                 nombres: name,
                 last_name: surname,
-                email: email,
+                email: email.toLocaleLowerCase(),
                 password: password,
                 numero_telefonico: phone,
                 url: url,
@@ -74,22 +93,24 @@ export default function SignupGAData() {
                 }
             });
             
-            routDocs(data);
+            //routDocs(data);
 
             console.log(data);
-            window.location.href = "/auth/login";
+            passStatus = true;
+            setErrMessage("Usuario registrado exitosamente");
+            setTimeout(() => { window.location.href = "/auth/login"; }, 1000);
         } 
         catch (error) {
             console.log(error.response.data);
+            passStatus = false;
+            setErrMessage("Hubo un error al registrarse");
         }
     };
 /*
-    useEffect(() => {
-    }, [first, second, third]);
-*/
     const routDocs = (data) => {
         router.push(`/providers/GA/documentosGA?_id=${data}`);
     }
+    */
 
     return (
         <>
@@ -106,8 +127,22 @@ export default function SignupGAData() {
                                             className="form-control"
                                             placeholder="Nombre(s)"
                                             value={name}
-                                            pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
-                                            onChange={(e) => setName(e.target.value)}
+                                            disabled={loading}
+                                            error={errors.name}
+                                            helperText={errors.name ? "Solo letras" : ""}
+                                            //pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
+                                            onChange={(e) => {
+                                                const v = e.target.value;
+                                                setName(v);
+                                                if (!/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test(v)) {
+                                                  setErrors({ ...errors, name: true })
+                                                } else {
+                                                  setErrors({ ...errors, name: false })
+                                                }
+                                              }}
+                                              sx={{
+                                                '& input': { padding: "0.8vw", marginRight: "0.1vw" },
+                                              }}
                                             required
                                         />
                                         <input
@@ -115,8 +150,21 @@ export default function SignupGAData() {
                                             className="form-control"
                                             placeholder="Apellidos"
                                             value={surname}
-                                            pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
-                                            onChange={(e) => setSurname(e.target.value)}
+                                            disabled={loading}
+                                            error={errors.surname}
+                                            helperText={errors.surname ? "Solo letras" : ""}
+                                            onChange={(e) => {
+                                                const v = e.target.value;
+                                                setSurname(v);
+                                                if (!/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test(v)) {
+                                                setErrors({ ...errors, surname: true })
+                                                } else {
+                                                setErrors({ ...errors, surname: false })
+                                                }
+                                            }}
+                                            sx={{
+                                                '& input': { padding: "0.8vw" },
+                                            }}
                                             required
                                         />
                                     </div>
@@ -127,7 +175,21 @@ export default function SignupGAData() {
                                         className="form-control"
                                         placeholder="Correo Electrónico"
                                         value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
+                                        disabled={loading}
+                                        error={errors.email}
+                                        helperText={errors.email ? "Correo electrónico inválido" : ""}
+                                        onChange={(e) => {
+                                        const v = e.target.value;
+                                        setEmail(v);
+                                        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v)) {
+                                            setErrors({ ...errors, email: true });
+                                        } else {
+                                            setErrors({ ...errors, email: false });
+                                        }
+                                        }}
+                                        sx={{
+                                        '& input': { padding: "0.8vw" },
+                                        }}
                                         required
                                     />
                                 </div>
@@ -137,7 +199,34 @@ export default function SignupGAData() {
                                         className="form-control"
                                         placeholder="Contraseña"
                                         value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
+                                        disabled={loading}
+                                        error={errors.password}
+                                        helperText={errors.password ? confPasswordHelper : ""}
+                                        onChange={(e) => {
+                                        const v = e.target.value;
+                                        setPassword(v);
+                                        if (v.length < 6 || !/(!|@|%|&|#|\*|\?|¿|¡|\$)+/.test(v) || !/\w/.test(v) || !/\d/.test(v)) {
+                                            setErrors({ ...errors, password: true })
+                                            if (v.length < 6) {
+                                            setConfPasswordHelper("La contraseña debe tener al menos 6 caracteres")
+                                            }
+                                            else if (!/[a-zA-Z]/.test(v)) {
+                                            setConfPasswordHelper("La contraseña debe tener al menos una letra")
+                                            }
+                                            else if (!/\d/.test(v)) {
+                                            setConfPasswordHelper("La contraseña debe tener al menos un digito")
+                                            }
+                                            else if (!/(!|@|%|&|#|\*|\?|¿|¡|\$)+/.test(v)) {
+                                            setConfPasswordHelper("La contraseña debe tener al menos un caracter especial")
+                                            }
+                                        } else {
+                                            setErrors({ ...errors, password: false })
+                                            setConfPasswordHelper("");
+                                        }
+                                        }}
+                                        sx={{
+                                        '& input': { padding: "0.8vw" },
+                                        }}
                                         required
                                     />
                                 </div>
@@ -146,6 +235,22 @@ export default function SignupGAData() {
                                         type="password"
                                         className="form-control"
                                         placeholder="Confirmar Contraseña"
+                                        value={confPassword}
+                                        disabled={loading}
+                                        error={errors.confPassword}
+                                        helperText={errors.confPassword ? "Las contraseñas no coinciden" : ""}
+                                        onChange={(e) => {
+                                        const v = e.target.value;
+                                        setConfPassword(v);
+                                        if (v !== password) {
+                                            setErrors({ ...errors, confPassword: true })
+                                        } else {
+                                            setErrors({ ...errors, confPassword: false })
+                                        }
+                                        }}
+                                        sx={{
+                                        '& input': { padding: "0.8vw" },
+                                        }}
                                         required
                                     />
                                 </div>
@@ -376,259 +481,5 @@ export default function SignupGAData() {
             )}
         </>
     );
-/*
-    useEffect(() => {
-    }, [first, second, third]);
-
-
-    const firstSection = () => {
-        setFirst(true);
-        setSecond(false);
-        setThird(false);
-    };
-
-    const secondSection = () => {
-        setFirst(false);
-        setSecond(true);
-        setThird(false);
-    };
-
-    const thirdSection = () => {
-        setFirst(false);
-        setSecond(false);
-        setThird(true);
-    };
-
-    return (
-        <div> 
-            {first && <div>
-                <h1>Registro de GA Admin</h1>
-*/
-
-
-    /*
-    return (
-        
-        <div> 
-            {first && <div>
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Nombre(s)"
-                    value={name}
-                    pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Apellido(s)"
-                    value={surname}
-                    pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
-                    onChange={(e) => setSurname(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Contraseña"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Confirmar contraseña"
-                    required
-                />
-
-                <button onClick={secondSection}>
-                    Siguiente
-                </button>
-            </div>}
-
-            {second && <div>
-                <h1>Registro de GA</h1>
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Nombre del GA"
-                    value={agencyName}
-                    onChange={(e) => setAgencyName(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="RFC"
-                    value={rfc}
-                    onChange={(e) => setRfc(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Calle"
-                    value={street}
-                    onChange={(e) => setStreet(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Numero exterior"
-                    value={exterior_num}
-                    onChange={(e) => setExteriorNum(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Numero interior"
-                    value={interior_num}
-                    onChange={(e) => setInteriorNum(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Ciudad"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Estado"
-                    value={state}
-                    onChange={(e) => setState(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="País"
-                    value={country}
-                    onChange={(e) => setCountry(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Código postal"
-                    value={postalCode}
-                    onChange={(e) => setPC(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-
-                    type="password"
-
-                    className="form-control"
-                    placeholder="Número telefónico"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    required
-                />                
-
-                <input
-                    type="text"
-
-                    type="password"
-                    className="form-control"
-                    placeholder="URL Sitio web"
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}
-                    required
-                />
-
-                <button onClick={firstSection}>
-                    Atrás
-                </button>
-
-                <button onClick={thirdSection}>
-                    Siguiente
-                </button>
-            </div>}
-
-            {third && <div>
-                <h1>Registro de GA Legal</h1>
-                <input
-
-            {third && <div><input
-                    type="text"
-                    className="form-control"
-                    placeholder="Nombre(s)"
-                    value={legalName}
-                    pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
-                    onChange={(e) => setLegalName(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Apellido(s)"
-                    value={legalSurname}
-                    pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
-                    onChange={(e) => setLegalSurname(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Email"
-                    value={legalEmail}
-                    onChange={(e) => setLegalEmail(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Teléfono"
-                    value={legalPhone}
-                    onChange={(e) => setLegalPhone(e.target.value)}
-                    required
-                />
-
-                <button onClick={secondSection}>
-                    Atrás
-                </button>
-
-                <button onClick={submitHandler}>
-                    Confirmar
-                </button>
-            </div>}
-        </div>
-    );
-}
-    );*/
 }
 
