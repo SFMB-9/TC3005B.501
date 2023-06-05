9ecea0d1a71d25f71f46c7abb30b27e6466e26cd
Ana Paula Katsuda <a01025303@tec.mx>
2023-06-05 17:27:13 -0600

    Corrected agency portal

--- App/swivel/src/pages/api/GA/pull-agencias.js
+++ App/swivel/src/pages/api/GA/pull-agencias.js
@@ -1,5 +1,5 @@
 import connectToDatabase from "@/utils/mongodb";
-import { encryptRole } from "../../../utils/crypto";
+// import { encryptRole } from "../../../utils/crypto";
 
 /* 
 Pulls all agencies depending on filters
@@ -10,18 +10,30 @@ Pending filters
 */
 
 export default async function handler(req, res) {
+  const client = await connectToDatabase;
+  const db = client.db("test");
+
+  // const { role, GA } = req.query;
+  const tipo_usuario = req.query.tipo_usuario;
+  const grupo_automotriz_id = req.query.grupo_automotriz_id;
+
   if (req.method === "GET") {
-    const { db } = await connectToDatabase();
-    const { role, GA } = req.query;
-
-    const encryptedRole = encryptRole(role);
-
-    const agencies = await db
-      .collection("usuarios")
-      .find({ role: role, grupo_automotriz: GA }) // change to role: encryptedRole
-      .toArray();
-    res.status(200).json(agencies);
-
+    try {
+      let result = await db.collection("usuarios").find({
+        tipo_usuario: tipo_usuario,
+        grupo_automotriz_id: grupo_automotriz_id
+      }).toArray();
+      if (result.length === 0) {
+        return res.status(404).json({ message: "No se encontraron agencias" });
+      }
+      if (!result) {
+        return res.status(500).json({ message: "Error al buscar agencias" });
+      }
+      // const encryptedRole = encryptRole(role)
+       return res.status(200).json({result: result, message: "Agencias encontradas" });
+    } catch (err) {
+      return res.status(400).json({ message: "Error al buscar agencias", error: err.message });
+    }
   } else {
     res.status(400).json({ message: "Wrong request method" });
   }
--- App/swivel/src/pages/providers/GA/agencias.jsx
+++ App/swivel/src/pages/providers/GA/agencias.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Agencias() {
   const [agencias, setAgencias] = useState([]);
-  const [filters, setFilters] = useState({
-    search: "",
-    page: 1,
-    limit: 10,
-  }); // Yet to add filters
+  const role = "ea32725caec36ffca1c1ee939e606cd1"; 
+  const GA = "647ae7c7f25041c1b7b8a57b";
+  // const [role, setRole] = useState("ea32725caec36ffca1c1ee939e606cd1");
+  // const [GA, setGA] = useState("647ae7c7f25041c1b7b8a57b");
+  // const [filters, setFilters] = useState([]); // Yet to add filters
 
   useEffect(() => {
     const fetchAgencias = async () => {
       const res = await fetch(
-        `http://localhost:3000/api/GA/pull-agencias`
+        `http://localhost:3000/api/GA/pull-agencias?tipo_usuario=${role}&grupo_automotriz_id=${GA}`
       );
       const data = await res.json();
-      setAgencias(data);
+      setAgencias(data.result);
     };
     fetchAgencias();
   }, []);
 
+  console.log(agencias)
   return (
     <div>
       <h1>Agencias</h1>
@@ -26,6 +27,7 @@ export default function Agencias() {
             <thead>
                 <tr>
                     <th>Nombre</th>
+                    <th>Estado</th>
                     <th>Numero</th>
                     <th>Email</th>
                 </tr>
@@ -33,7 +35,8 @@ export default function Agencias() {
             <tbody>
                 {agencias && agencias.map((agencia) => (
                     <tr key={agencia._id}>
-                        <td>{agencia.nombre}</td>
+                        <td>{agencia.nombres}</td>
+                        <td>{agencia.direccion.estado}</td>
                         <td>{agencia.numero_telefonico}</td>
                         <td>{agencia.email}</td>
                         <td>
