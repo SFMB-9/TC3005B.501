9047897c892e2de814750c4dffee268c36919b1a
SeisshinKB <75494927+ivalani@users.noreply.github.com>
2023-06-05 18:48:00 -0600

    double redirection fix

--- App/swivel/src/components/providers/GA/navbar.jsx
+++ App/swivel/src/components/providers/GA/navbar.jsx
@@ -5,8 +5,8 @@ export default function GANavbar() {
     <CustomNavbar
       home='/providers/GA'
       elems_right={[
-        {name: 'Acciones', href: '', popup: [{name: 'Manejo de catálogo', href: ''}, {name: 'Administración de gerentes', href: 'GA/manageGA'}, {name: 'Administración de la agencias', href: 'GA/portal_agencias'}, {name: 'Estadísticas de ventas', href: ''}]},
-        {name: 'Mi cuenta', href: '', popup: [{name: 'Ver mi perfil', href: 'GA/editProfile'}, {name: 'Cambiar contraseña', href: '/providers/GA/GA-update-password'}, {name: 'Cerrar sesión', href:'#', signoutComponent: '/auth/login'}]},
+        {name: 'Acciones', href: '', popup: [{name: 'Administración de la agencias', href: '../providers/GA/portal_agencias'}, {name: 'Estadísticas de ventas', href: ''}]},
+        {name: 'Mi cuenta', href: '', popup: [{name: 'Ver mi perfil', href: '../GA/editProfile'}, {name: 'Cambiar contraseña', href: '../GA/GA-update-password'}, {name: 'Cerrar sesión', href:'#', signoutComponent: '/auth/login'}]},
       ]}
     />
   )
