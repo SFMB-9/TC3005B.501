21b4924ec27bc6051d54b2af198867b71e9ac8f7
= <andyserrano_d@outlook.com>
2023-06-07 20:23:37 -0600

    Pruebas manejo done, and changes in playwright con

--- IntegraTest/playwright.config.js
+++ IntegraTest/playwright.config.js
@@ -22,6 +22,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 30000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
