73cc6460a0fb268da193106ad184e6927e24cceb
karla-mond <karla.mondragon.rosas@gmail.com>
2023-06-08 14:13:54 -0600

    proceso GA

---
+++ App/swivel/src/components/providers/GA/process.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Step from "@/components/providers/GA/step";
+import Image from "next/image";
+import { Typography, Button } from "@mui/material";
+import Link from "next/link";
+
+// Funcion que retorna el componente Process
+const Process = () => {
+  return (
+    <section style={{backgroundColor: '#000'}}>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+        }}
+      >
+        <div className="container p-5" >
+          <div>
+            <Typography
+              sx={{
+                color: "white",
+              }}
+              fontSize={40}
+              fontFamily="Raleway"
+              align="left"
+            >
+              ¡Trabaja con Nosotrxs!
+            </Typography>
+          </div>
+
+          <div className="row">
+            <div className="col-lg col-md-6 mt-3">
+              <Step
+                number="1"
+                img="/processGA1.svg"
+                text="Regístrate como usuario"
+              />
+            </div>
+
+            <div className="col-lg col-md-6 mt-3">
+              <Step
+                number="2"
+                img="/processGA2.svg"
+                text="Registra a tu Grupo Automotriz"
+              />
+            </div>
+
+            <div className="col-lg col-md-6 mt-3">
+              <Step
+                number="3"
+                img="/processGA3.svg"
+                text="Sube tus documentos"
+              />
+            </div>
+
+            <div className="col-lg col-md-6 mt-3">
+              <Step
+                number="4"
+                img="/processGA4.svg"
+                text="Bienvenidx a SWIVEL"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Process;
--- App/swivel/src/pages/providers/GA/landing.js
+++ App/swivel/src/pages/providers/GA/landing.js
@@ -1,16 +1,17 @@
 import React from "react";
 import CustomHero from "@/components/general/custom_hero";
 import OtherGANavbar from "@/components/providers/GA/navNoSession";
-
+import Process from "@/components/providers/GA/process";
 
 export default function gaLandingPage() {
   return (
     <>
       <OtherGANavbar />
       <CustomHero
-        title="Bienvenidx, nombre"
-        message="Administra tus solicitudes pendientes"
+        title="Se parte del cambio"
+        message="¡Unete a Swivel! Regístrate como Grupo Automotriz y se parte del cambio"
       />
+      <Process />
     </>
   );
 }
