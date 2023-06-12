25888196d88affc144d2232cfb2287b298720d5e
Sebastián González <a01029746@itesm.mx>
2023-06-11 22:31:52 -0600

    Removed GA id existent condition from rendering

--- App/swivel/src/pages/providers/GA/registroAdmin.js
+++ App/swivel/src/pages/providers/GA/registroAdmin.js
@@ -19,23 +19,23 @@ const RegistroAdmin = () => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [phone, setPhone] = useState("");
-    const [errors, setErrors] = useState({}); 
+    const [errors, setErrors] = useState({});
     const [GA, setGA] = useState("");
 
     useEffect(() => {
         const getIdGA = async () => {
-          let gaIdRaw = await fetch(`http://localhost:3000/api/GA/GA-get-id?_id=${session.id}`,
-            { method: 'GET' });
-    
-          const gaId = await gaIdRaw.json();
-    
-          return gaId.user.grupo_automotriz_id;
+            let gaIdRaw = await fetch(`/api/GA/GA-get-id?_id=${session.id}`,
+                { method: 'GET' });
+
+            const gaId = await gaIdRaw.json();
+
+            return gaId.user.grupo_automotriz_id;
         }
-    
+
         if (router.isReady && session) {
-          getIdGA().then((ga_id) =>
-            setGA(ga_id)
-          );
+            getIdGA().then((ga_id) =>
+                setGA(ga_id)
+            );
         }
     }, [router.isReady, session]);
 
@@ -104,154 +104,146 @@ const RegistroAdmin = () => {
 
     const handleCancel = () => {
     };
-
-    if (GA){
-        return (
-            <div>
-                <GANavbar />
-                <div className={styles.mainContainer}>
-                    <h1 className={styles.pageTitle}>Registrar administrador de GA</h1>
-                    <h3 className={styles.boldText}>Ingresa los datos del administrador</h3>
-                    <form onSubmit={submitHandler}>
-                        <div className={styles.inputContainer}>
-                            <div className={styles.row}> 
-                                <div className={styles.inputFieldContainer}>
-                                    <h5>Nombre(s)</h5>
-                                    <TextField
-                                        className={styles.inputField}
-                                        type="text"
-                                        //className="form-control" no aplica 
-                                        placeholder="Nombre(s)"
-                                        fullWidth
-                                        value={name}
-                                        pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
-                                        onChange={(e) => setName(e.target.value)}
-                                        required
-                                        error={!!errors.nombres}
-                                        helperText={errors.nombres}
-                                    />                               
-                                </div>
-                                <div className={styles.inputFieldContainer}>
-                                    <h5>Apellido(s)</h5>
-                                    <TextField
-                                            className={styles.inputField}
-                                            type="text"
-                                            //className="form-control" no aplica 
-                                            placeholder="Apellido(s)"
-                                            fullWidth
-                                            value={surname}
-                                            pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
-                                            onChange={(e) => setSurname(e.target.value)}
-                                            required
-                                            error={!!errors.nombres}
-                                            helperText={errors.nombres}
-                                        />
-                                </div>
+    
+    return (
+        <div>
+            <GANavbar />
+            <div className={styles.mainContainer}>
+                <h1 className={styles.pageTitle}>Registrar administrador de GA</h1>
+                <h3 className={styles.boldText}>Ingresa los datos del administrador</h3>
+                <form onSubmit={submitHandler}>
+                    <div className={styles.inputContainer}>
+                        <div className={styles.row}>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Nombre(s)</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    type="text"
+                                    //className="form-control" no aplica 
+                                    placeholder="Nombre(s)"
+                                    fullWidth
+                                    value={name}
+                                    pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
+                                    onChange={(e) => setName(e.target.value)}
+                                    required
+                                    error={!!errors.nombres}
+                                    helperText={errors.nombres}
+                                />
+                            </div>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Apellido(s)</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    type="text"
+                                    //className="form-control" no aplica 
+                                    placeholder="Apellido(s)"
+                                    fullWidth
+                                    value={surname}
+                                    pattern="[a-zA-Z0-9À-ÿ\u00f1\u00d1\s]+"
+                                    onChange={(e) => setSurname(e.target.value)}
+                                    required
+                                    error={!!errors.nombres}
+                                    helperText={errors.nombres}
+                                />
+                            </div>
                         </div>
                     </div>
                     <div className={styles.inputContainer}>
-                            <div className={styles.inputFieldContainer}>
-                                    <h5>Email</h5>
-                                    <TextField
-                                        className={styles.longInputField}
-                                        type="text"
-                                        //className="form-control"
-                                        placeholder="Email"
-                                        variant="outlined"
-                                        fullWidth
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        error={!!errors.correo}
-                                        helperText={errors.correo}
-                                        required
-                                    />
-                            </div>
+                        <div className={styles.inputFieldContainer}>
+                            <h5>Email</h5>
+                            <TextField
+                                className={styles.longInputField}
+                                type="text"
+                                //className="form-control"
+                                placeholder="Email"
+                                variant="outlined"
+                                fullWidth
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                error={!!errors.correo}
+                                helperText={errors.correo}
+                                required
+                            />
+                        </div>
                     </div>
                     <div className={styles.inputContainer}>
-                            <div className={styles.inputFieldContainer}>
-                                    <h5>Teléfono</h5>
-                                    <TextField
-                                        className={styles.longInputField}
-                                        type="text"
-                                        //className="form-control"
-                                        placeholder="Teléfono"
-                                        variant="outlined"
-                                        fullWidth
-                                        value={phone}
-                                        onChange={(e) => setPhone(e.target.value)}
-                                        error={!!errors.telefono}
-                                        helperText={errors.telefono}
-                                        required
-                                    />
-                            </div>
+                        <div className={styles.inputFieldContainer}>
+                            <h5>Teléfono</h5>
+                            <TextField
+                                className={styles.longInputField}
+                                type="text"
+                                //className="form-control"
+                                placeholder="Teléfono"
+                                variant="outlined"
+                                fullWidth
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                                error={!!errors.telefono}
+                                helperText={errors.telefono}
+                                required
+                            />
+                        </div>
                     </div>
-    
+
                     <h3 className={styles.boldText}>Crear una contraseña</h3>
                     <div className={styles.inputContainer}>
-                            <div className={styles.row}>
-                                <div className={styles.inputFieldContainer}>
-                                    <h5>Contraseña</h5>
-                                    <TextField
-                                        className={styles.inputField}
-                                        type="password"
-                                        placeholder="Contraseña"
-                                        variant="outlined"
-                                        fullWidth
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        error={!!errors.contraseña}
-                                        helperText={errors.contraseña}
-                                        required
-                                        //type="password"
-                                    />
-                                </div>
-                                <div className={styles.inputFieldContainer}>
-                                    <h5>Confirmar Contraseña</h5>
-                                    <TextField
-                                        className={styles.inputField}
-                                        type="password"
-                                        placeholder="Contraseña"
-                                        variant="outlined"
-                                        fullWidth
-                                        error={!!errors.confirmarContraseña}
-                                        helperText={errors.confirmarContraseña}
-                                        onChange={(e) => setConfirmPassword(e.target.value)}
-                                        //type="password"
-                                        required
-                                    />
-                                </div>
+                        <div className={styles.row}>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Contraseña</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    type="password"
+                                    placeholder="Contraseña"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    error={!!errors.contraseña}
+                                    helperText={errors.contraseña}
+                                    required
+                                //type="password"
+                                />
+                            </div>
+                            <div className={styles.inputFieldContainer}>
+                                <h5>Confirmar Contraseña</h5>
+                                <TextField
+                                    className={styles.inputField}
+                                    type="password"
+                                    placeholder="Contraseña"
+                                    variant="outlined"
+                                    fullWidth
+                                    error={!!errors.confirmarContraseña}
+                                    helperText={errors.confirmarContraseña}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    //type="password"
+                                    required
+                                />
                             </div>
                         </div>
+                    </div>
                     <div className={styles.buttonContainer}>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                type="submit"
-                                className={styles.button}
-                            >
-                                Register
-                            </Button>
-                            <Button
-                                variant="outlined"
-                                color="primary"
-                                onClick={handleCancel}
-                                className={styles.button}
-                            >
-                                Cancel
-                            </Button>
-                        </div>
-                    </form>
-                </div>    
-                </div>
-                
-        );
-    } else {
-        return (
-            <div>
-                <p>Cargando...</p>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            className={styles.button}
+                        >
+                            Register
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            onClick={handleCancel}
+                            className={styles.button}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </form>
             </div>
-        );
-    }
+        </div>
+
+    );
 }
 
 export default RegistroAdmin;
