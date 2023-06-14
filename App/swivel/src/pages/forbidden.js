7ca8b58dedb137aa053e03ada9a522dbf92e7134
Andreina Sananez <asananez2000@gmail.com>
2023-06-13 20:20:42 -0600

    Add forbidden page

---
+++ App/swivel/src/pages/forbidden.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Forbidden = () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>403 - Forbidden</h1>
+    <p>You do not have permission to access this page.</p>
+  </div>
+);
+
+export default Forbidden;
