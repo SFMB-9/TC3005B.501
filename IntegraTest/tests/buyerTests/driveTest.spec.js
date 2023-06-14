ef02fd477d100515f628aeec36276085f3962f37
= <andyserrano_d@outlook.com>
2023-06-13 18:36:54 -0600

    viewProfile test

--- IntegraTest/tests/buyerTests/driveTest.spec.js
+++ IntegraTest/tests/buyerTests/driveTest.spec.js
@@ -9,30 +9,33 @@ test.describe('Driving Req', () => {
 
         //Se inicia y confirma la sesión antes de realizar el proceso
         await page.locator('input[type="email"]').click();
-        await page.locator('input[type="email"]').fill('karla@gmail.com');
+        await page.locator('input[type="email"]').fill('email4@test.com');
         await page.locator('input[type="email"]').press('Tab');
-        await page.locator('input[type="password"]').fill('Karla123!');
+        await page.locator('input[type="password"]').fill('12345!Q');
         await page.locator('button', { name: 'Ingresar' }).hover();
         await page.getByRole('button', { name: 'Ingresar' }).click();
-        setTimeout(() => { page.getByRole('menuitem', { name: 'Mi cuenta' }).getByText('Mi cuenta').hover();}, 50000);
+        await page.locator('button').click();
+        await page.locator('button').click();
     })
 
     test('Agendar prueba de manejo correctamente', async ({ page }) => {
-        setTimeout(() => { page.getByRole('banner').getByRole('link', { name: 'Compra un auto' }).click();}, 50000);
-        setTimeout(() => { page.getByRole('link', { name: 'Nissan Versa 2019 · Ciudad de México · Nissan Cuauhtémoc Disponible en 2 colores $245.999 MXN' }).click();}, 50000); 
-        setTimeout(() => { page.getByRole('button', { name: 'Prueba de manejo' }).click();}, 50000); 
-        setTimeout(() => { page.goto('http://localhost:3000/catalog/test-detail?auto_id=kZS_dIgB6Fc17-h0kMHn&colorName=Blanco+Nieve');}, 50000); 
-        setTimeout(() => { page.getByRole('button', { name: 'Continuar' }).click();}, 50000);
-        setTimeout(() => { page.getByRole('textbox').first().click();}, 50000); 
-        setTimeout(() => { page.getByRole('option', { name: 'Choose Thursday, June 15th, 2023' }).click();}, 50000); 
-        setTimeout(() => { page.getByRole('textbox').nth(1).click();}, 50000); 
-        setTimeout(() => { page.getByText('06 PM').click();}, 50000); 
-        setTimeout(() => { page.getByRole('button', { name: 'Continuar' }).click();}, 50000); 
-        setTimeout(() => { page.getByRole('button', { name: 'Confirmar' }).click();}, 50000); 
-        setTimeout(() => { page.getByRole('menuitem', { name: 'Mi cuenta' }).click();}, 50000);
-        setTimeout(() => { page.getByRole('menuitem', { name: 'Ver mi perfil' }).click();}, 50000); 
-        setTimeout(() => { page.getByRole('link', { name: 'Mis pruebas de manejo' }).getByTestId('ps-menu-button-test-id').click();}, 50000); 
-        setTimeout(() => { page.getByRole('link', { name: 'Nissan Versa 2019 Nissan Cuauhtemoc Fecha de la cita: 15 de junio del 2023 Horario de la cita: 18:00 Estatus: En proceso' }).click();}, 50000); 
-        setTimeout(() => { page.locator('div').filter({ hasText: /^¡Su prueba de manejo ha sido agendada exitosamente!$/ }).click();}, 50000);     
+        await page.getByRole('menuitem', { name: 'Mi cuenta' }).getByText('Mi cuenta').hover();
+        await page.getByRole('menuitem', { name: 'Mi cuenta' }).getByText('Mi cuenta').click();
+        await page.getByRole('banner').getByRole('link', { name: 'Compra un auto' }).click();
+        await page.getByRole('link', { name: 'Nissan Versa 2019 · Ciudad de México · Nissan Benito Juárez Disponible en 2 colores $245.999 MXN' }).click(); 
+        await page.getByRole('button', { name: 'Prueba de manejo' }).click(); 
+        await page.goto('http://localhost:3000/catalog/test-detail?auto_id=kZS_dIgB6Fc17-h0kMHn&colorName=Blanco+Nieve'); 
+        await page.getByRole('button', { name: 'Continuar' }).click();
+        await page.getByRole('textbox').first().click(); 
+        await page.getByRole('option', { name: 'Choose Thursday, June 15th, 2023' }).click(); 
+        await page.getByRole('textbox').nth(1).click();
+        await page.getByText('06 PM').click(); 
+        await page.getByRole('button', { name: 'Continuar' }).click(); 
+        await page.getByRole('button', { name: 'Confirmar' }).click(); 
+        await page.getByRole('menuitem', { name: 'Mi cuenta' }).click();
+        await page.getByRole('menuitem', { name: 'Ver mi perfil' }).click();
+        await page.getByRole('link', { name: 'Mis pruebas de manejo' }).getByTestId('ps-menu-button-test-id').click();
+        await page.getByRole('link', { name: 'Nissan Versa 2019 Nissan Cuauhtemoc Fecha de la cita: 15 de junio del 2023 Horario de la cita: 18:00 Estatus: En proceso' }).click(); 
+        await page.locator('div').filter({ hasText: /^¡Su prueba de manejo ha sido agendada exitosamente!$/ }).click();    
     })
 })
--- IntegraTest/tests/buyerTests/viewProfile.spec.js
+++ IntegraTest/tests/buyerTests/viewProfile.spec.js
@@ -1,2 +1,23 @@
 import { test, expect } from '@playwright/test'
 import { timeout } from '../../playwright.config'
+
+test.describe
+
+test('test', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.getByRole('link', { name: 'Iniciar sesión' }).click();
+    await page.locator('input[type="email"]').click();
+    await page.locator('input[type="email"]').fill('karla@gmail.com');
+    await page.locator('input[type="email"]').press('Tab');
+    await page.locator('input[type="password"]').press('CapsLock');
+    await page.locator('input[type="password"]').fill('K');
+    await page.locator('input[type="password"]').press('CapsLock');
+    await page.locator('input[type="password"]').fill('Karla123!');
+    await page.getByRole('button', { name: 'Ingresar' }).click();
+    await page.getByRole('menuitem', { name: 'Mi cuenta' }).getByText('Mi cuenta').click();
+    await page.getByRole('link', { name: 'Ver mi perfil' }).click();
+    await page.getByText('Editar datos').click();
+    await page.locator('input[name="numero int"]').click();
+    await page.locator('input[name="numero int"]').fill('234');
+    await page.getByRole('button', { name: 'Guardar datos' }).click();
+});
