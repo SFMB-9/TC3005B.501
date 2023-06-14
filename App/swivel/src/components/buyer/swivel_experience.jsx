239c332d66a3d888f46847117f67d661834df4fc
SeisshinKB <75494927+ivalani@users.noreply.github.com>
2023-06-14 02:33:46 -0600

    button layout

--- App/swivel/src/components/buyer/swivel_experience.jsx
+++ App/swivel/src/components/buyer/swivel_experience.jsx
@@ -1,6 +1,4 @@
-import {
-  Button,
-} from "@mui/material";
+import {Button} from "@mui/material";
 import styles from '@/styles/swivel_experience.module.css'
 import { Height } from "@mui/icons-material";
 
@@ -17,23 +15,27 @@ export default function SwivelExperience() {
             Realizar compras en SWIVEL es seguro, simple y rápido. ¡Inténtalo ya!
           </span>
           <br />
-          <Button
-            variant="contained"
-            type="submit"
-            className="w-80"
-            sx={{
-              fontFamily: "Lato",
-              ":hover": {
-                backgroundColor: "#333333", },
-              border: 'none',
-              marginTop: '2.5rem',
-              width: 'auto',
-              alignItems: "center"
-            }}
-            href="/auth/signup"
-          >
-            <h5> Regístrate </h5>
-          </Button>
+          <div className={styles.buttomContainer}> 
+            <Button
+              variant="contained"
+              type="submit"
+              className="w-80"
+              alignItems = "center"
+              sx={{
+                fontFamily: "Lato",
+                ":hover": {
+                  backgroundColor: "#333333", },
+                border: 'none',
+                marginTop: '2.5rem',
+                width: 'auto',
+                alignItems: "center"
+              }}
+              href="/auth/signup"
+            >
+              
+              <h5> Regístrate aquí </h5>
+            </Button> 
+          </div>
         </div>
       </div>
 
@@ -44,23 +46,25 @@ export default function SwivelExperience() {
             Busca a partir de tus necesidades, navega por nuestro amplio catálogo y utiliza nuestros filtros para acotar tu búsqueda. Una vez que hayas seleccionado el auto de tus sueños, elige el plan de financiamiento que se adapte a ti y haz tu primer pago. 
           </span>
           <br />
-          <Button
-            variant="contained"
-            type="submit"
-            className="w-80"
-            sx={{
-              fontFamily: "Lato",
-              ":hover": {
-                backgroundColor: "#333333", },
-              border: 'none',
-              marginTop: '2.5rem',
-              width: 'auto',
-              alignItems: "center"
-            }}
-            href="/"
-          >
-            <h5> Descubre el auto para ti </h5>
-          </Button>
+          <div className={styles.buttomContainer}> 
+            <Button
+              variant="contained"
+              type="submit"
+              className="w-80"
+              sx={{
+                fontFamily: "Lato",
+                ":hover": {
+                  backgroundColor: "#333333", },
+                border: 'none',
+                marginTop: '2.5rem',
+                width: 'auto',
+                alignItems: "center"
+              }}
+              href="/catalog"
+            >
+              <h5> Descubre el auto para ti </h5>
+            </Button>
+          </div>
         </div>
         <img src='/buyer/exp2.png'/>
       </div>
