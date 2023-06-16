cff8f220274bc99f761c53ad85f812d9e547f6c1
Ace0x <59582360+Ace0x@users.noreply.github.com>
2023-06-16 09:07:31 -0700

    removed model

--- AI_Model/load.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import tensorflow as tf
-
-load_model = tf.keras.models.load_model("mod.h5")
-x = np.array([1])
-y = np.array([1])
-predict = load_model.predict([x,y])
-print(predict)
